7f55e66a5bdd97a5e60167d34d33b550755a7b5c
Sriee <psg.sriee@gmail.com>
2017-06-12 19:39:11 -0500

    Parity checker implementation and test case

---
+++ Interview/src/com/epi/primitives/Test.java
@@ -0,0 +1,11 @@
+package com.epi.primitives;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(Long.MAX_VALUE);
+		long num = Long.MAX_VALUE;
+		System.out.println(num);		
+	}
+
+}
