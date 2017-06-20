acda75fea401cb6b54b5d5bf7b4f8908cdbdfdfd
Sriee <psg.sriee@gmail.com>
2017-06-20 00:00:24 -0500

    Base Conversion between indexes

---
+++ Interview/src/com/epi/primitives/BaseConversion.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.epi.primitives;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author sriee
+ *
+ */
+public class BaseConversion {
+
+	public String baseConversion(int base1, String input, int base2){
+		return null;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		try{
+			String input = scan.next();
+			
+		} catch (InputMismatchException e){
+			
+		} finally {
+			scan.close();
+		}
+	}
+
+}
