a298d1a3e0ceb2720ccc4e6528f72d34173f2d64
Sriee <psg.sriee@gmail.com>
2017-07-04 11:46:19 -0500

    Added Test suite to run all the test cases in a package

---
+++ Interview/test/com/test/primitives/PrimitivesTestSuite.java
@@ -0,0 +1,20 @@
+package com.test.primitives;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+		BaseConversionTest.class,
+		DecodeExcelTest.class,
+		ParityTest.class,
+		PowerSetTest.class,
+		ReverseBitsTest.class,
+		StringIntTest.class,
+		SwapBitsTest.class
+})
+
+public class PrimitivesTestSuite {
+
+}
