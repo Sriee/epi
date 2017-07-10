a87e3a8e02aef9e36f60e43ba4c6e2a2a487b1a7
Sriee <psg.sriee@gmail.com>
2017-07-10 08:32:57 -0500

    Formatting the output

--- Report/performance.py
+++ Report/performance.py
@@ -1,93 +1,141 @@
 import os
 import json
 
+
 class Performance(object):
+    def __init__(self, uuid):
+        self.uuid = uuid
+        self.test_case = {}
+        self._names = []
+        self._test_type = ''
+        self.load_object()
 
-	def __init__(self, uuid):
-		self.uuid = uuid
-		self.test_case = {}
-		self.load_object()
+    def load_object(self):
+        with open(os.path.join('Performance', self.uuid), 'r') as rp:
+            inp = json.load(rp)
 
-	def load_object(self):
+        self.test_type = inp['test_type']
+        for item in inp['testcases']:
+            name = item['name']
+            self.test_case[name] = {}
+            self.test_case[name]['status'] = item['status']
+            self.test_case[name]['item_id'] = item['item_id']
+            self.test_case[name]['iterations'] = item['iterations']
+            self.test_case[name]['total_time'] = item['total_time']
+        else:
+            print('Object loaded')
 
-		with open(os.path.join('Performance', self.uuid), 'r') as rp:
-			inp = json.load(rp)
-		
-		for item in inp['testcases']:
-		    name = item['name']
-		    self.test_case[name]={}
-		    self.test_case[name]['status'] = item['status']
-		    self.test_case[name]['item_id'] = item['item_id']
-		    self.test_case[name]['iterations'] = item['iterations']
-		    self.test_case[name]['total_time'] = item['total_time']
-		else:
-			print 'Object loaded'
+    def __contains__(self, item):
+        return item in self.test_case
 
-	def __contains__(self, item):
-		return item in self.test_case
+    def __len__(self):
+        if self.test_case is None:
+            return 0
+        else:
+            return len(self.test_case)
 
-	def __len__(self):
-		if self.test_case is None:
-			return 0
-		else:
-			return len(self.test_case)
+    def __iter__(self):
+        self.index = -1
+        self.names = self.test_case.keys()
+        return self
 
-	def __iter__(self):
-		self.index = -1
-		self.keys = self.test_case.keys()
-		return self
+    def next(self):
+        try:
+            while self.index < self.__len__():
+                self.index += 1
+                return self.names[self.index]
+        except IndexError:
+            raise StopIteration
 
-	def next(self):
-		try:
-			while self.index < self.__len__:
-				self.index += 1
-				return self.keys[self.index]
-		except IndexError:
-			raise StopIteration
+    def __getitem__(self, item):
+        return self.test_case[item] if item in self.test_case else None
 
-	def __getitem__(self, item):
-		print 'getitem called'
-		return self.test_case[item] if item in self.test_case else None 
+    def get_keys(self):
+        return self.test_case.keys()
 
-	def get_keys(self):
-		return self.test_case.keys()
+    @property
+    def names(self):
+        return self._names
 
-	def __str__(self):
-		return 'Performance(uuid: {}, Test_cases: {}' \
-				.format(self.uuid, self.test_case.keys() if self.test_case else 'Empty')
+    @names.setter
+    def names(self, value):
+        self._names = value
 
-	def __repr__(self):
-		return str(self.test_case)
-"""
+    @property
+    def test_type(self):
+        return self._test_type
+
+    @test_type.setter
+    def test_type(self, value):
+        self._test_type = value
+
+    def __str__(self):
+        return 'Performance(uuid: {}, Test_cases: {}'.format(self.uuid,
+                                                             self.test_case.keys() if
+                                                             self.test_case else 'Empty')
+
+    def __repr__(self):
+        return str(self.test_case)
+
+
 def compare(first, second):
-	first_set, second_set = set(), set()
+    first_set, second_set, lim = set(first.get_keys()), set(second.get_keys()), '-' * 85
+    left, right, lines = None, None, []
+    lines.append(lim)
+    lines.append('| {:^9} | {:^40} | {:^11} | {:^11} {:>2} '
+                 .format('Item Id', 'Test Case Name', str(first.test_type).title(),
+                         str(second.test_type).title(), '|'))
+    lines.append(lim)
+    # Finding the intersection
+    common = first_set.intersection(second_set)
 
+    for element in common:
 
-	first_set = first.get_keys()
-	second_set = second.get_keys()
+        if first[element]['status'] == 'FAILED':
+            left = 'None'
+        else:
+            left = first[element]['total_time'] // first[element]['iterations']
 
-	# Finding the intersection
-	common = first_set.intersection(second_set)
+        if second[element]['status'] == 'FAILED':
+            right = 'None'
+        else:
+            right = second[element]['total_time'] // second[element]['iterations']
 
-	print '*' * 50
+        each_line = '| {}-{} | {:40} | {:<11} | {:<11} {:>2}' \
+            .format(first[element]['item_id'], second[element]['item_id'], element,
+                    left, right, '|')
+        lines.append(each_line)
 
-	if len(common) != 0:
-		for element in common:
-			print '{}'
-		pass # Do the comparison here 
+    # Finding the difference
+    diff = list(first_set ^ second_set)
 
-	# Finding the difference
-	diff = list(first_set ^ second_set)
+    for element in diff:
+        if element in first:
+            if first[element]['status'] == 'FAILED':
+                left = 'None'
+            else:
+                left = first[element]['total_time'] // first[element]['iterations']
 
-	for element in diff:
-		if element in first:
-"""
+            each_line = '| {}-{} | {:40} | {:<11} | {:<11} {:>2}' \
+                .format(first[element]['item_id'], 'None', element, left, 'None', '|')
+            lines.append(each_line)
+        else:
+            if second[element]['status'] == 'FAILED':
+                right = 'None'
+            else:
+                right = first[element]['total_time'] // first[element]['iterations']
 
-def pretty_print():
-	print 'l' + '-' * 100
+            each_line = '| {}-{} | {:40} | {:<11} | {:<11} {:>2}' \
+                .format('None', second[element]['item_id'], element, 'None', right, '|')
+            lines.append(each_line)
+
+    lines.append(lim)
+
+    for l in lines:
+        print(l)
 
 
 if __name__ == '__main__':
-	first = Performance('DHCPv4_smoke.json')
- 	second = Performance('DHCPv4_smoke_pim.json')
-	pretty_print()
+    first = Performance('http2_smoke.json')
+    second = Performance('http2_minisoak.json')
+    compare(first, second)
