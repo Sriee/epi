16e9dd4dd936b53617652e04acd95e17ab205c23
sriee <sriee.ammapet@spirent.com>
2017-08-01 15:21:46 -0500

    Completed iterator, made changes to variables so that they could be used on iteration

--- Interview/src/util/range/Range.java
+++ Interview/src/util/range/Range.java
@@ -1,13 +1,20 @@
 package util;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Range{
+public class Range implements Iterable<Integer>, Iterator<Integer>{
 
-	public Range(){}
+    private int index = 0;
+    private int current = Integer.MIN_VALUE;
+    private static int start = -1;
+    private static int stop = -1;
+	private static int step = -1;
+
+    public Range(){}
 	
-	private static List<Integer> generateRange(int start, int stop, int step){
+	private static List<Integer> generateRange(){
 		List<Integer> list = new ArrayList<Integer>();
 		for(int i = start; i < stop; i+= step){
 			list.add(i);
@@ -16,18 +23,42 @@ public class Range{
 	}
 	
 	public static List<Integer> range(int stop){
-		return generateRange(0, stop, 1);
+		Range.start = 0; Range.stop = stop; Range.step = 1;
+	    return generateRange();
 	}
 	
 	public static List<Integer> range(int start, int stop){
-		return generateRange(start, stop, 1);
+        Range.start = start; Range.stop = stop; Range.step = 1;
+	    return generateRange();
 	}
 	
 	public static List<Integer> range(int start, int stop, int step){
 		if(step < 1 )
 			throw new IllegalArgumentException("Step shoule be >= 1");
-		
-		return generateRange(start, stop, step);
+
+		Range.start = start; Range.stop = stop; Range.step = step;
+
+		return generateRange();
 	}
-	
+
+	@Override
+    public Iterator<Integer> iterator(){ return this; }
+
+    @Override
+    public boolean hasNext() { return this.index < Range.stop; }
+
+    @Override
+    public Integer next() {
+        if(this.current == Integer.MIN_VALUE){
+            this.current = Range.start;
+        } else {
+            this.current += Range.step;
+        }
+
+        this.index++;
+        return this.current;
+    }
+
+    @Override
+    public void remove() { throw new UnsupportedOperationException("Cannot Remove values while iterating."); }
 }
