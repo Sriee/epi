c3aaa6b210040c90930571d1736bbd00bb0e77b3
Sriee <psg.sriee@gmail.com>
2017-07-31 22:37:10 -0500

    Print Array utility usage

--- Interview/src/com/epi/array/Test.java
+++ Interview/src/com/epi/array/Test.java
@@ -1,11 +1,26 @@
-package com.epi.primitives;
+package com.epi.array;
+
+import java.util.Arrays;
 
 public class Test {
 
+	private static void print(int arr[]){
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	private static void print(double arr[]){
+		System.out.println(Arrays.toString(arr));
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(Long.MAX_VALUE);
-		long num = Long.MAX_VALUE;
-		System.out.println(num);		
+		int[] intArr = {1, 2, 3, 4};
+		double[] doubleArr = {2.4, 4.654, 3.564};
+		
+		print(intArr);
+		print(doubleArr);
+		
+//		for(int i = 0; i < 5; i++)
+//			System.out.print(dummy[i] + " ");
 	}
 
 }
