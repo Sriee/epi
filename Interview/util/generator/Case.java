7dd512272c21f466cb2351109c84f22db2640cc0
sriee <sriee.ammapet@spirent.com>
2017-08-01 10:25:49 -0500

    Moving utility files

--- Interview/util/generator/Case.java
+++
@@ -1,7 +0,0 @@
-package generator.number;
-
-public enum Case {
-    LOWER_CASE,
-    UPPER_CASE,
-    TITLE_CASE
-}
--- Interview/util/generator/Generator.java
+++
@@ -1,9 +0,0 @@
-package generator.number;
-
-import java.util.List;
-
-public interface Generator<T> {
-
-    public T generate(int size);
-    public List<T> generateAsList(int size);
-}
