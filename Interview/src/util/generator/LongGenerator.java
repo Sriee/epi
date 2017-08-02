f56108e12793e99de024385b679cc3dbab42b773
Sriee <psg.sriee@gmail.com>
2017-08-01 19:52:57 -0500

    Removed unused random

--- Interview/src/util/generator/LongGenerator.java
+++ Interview/src/util/generator/LongGenerator.java
@@ -3,7 +3,6 @@ package util.generator;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class LongGenerator implements Iterable<Long>, Iterator<Long>, Generator {
@@ -12,7 +11,6 @@ public class LongGenerator implements Iterable<Long>, Iterator<Long>, Generator 
     private long max = Long.MAX_VALUE;
     private long size = 0;
     private long index = 0;
-    private Random rand = null;
 
     public LongGenerator() {
         this.min = 0;
@@ -29,14 +27,10 @@ public class LongGenerator implements Iterable<Long>, Iterator<Long>, Generator 
 
         if (size < 0)
             throw new IllegalArgumentException("Size should not be <= 0");
-
         long result[] = new long[size];
-        Random rand = new Random();
-
         for (long i = 0; i < size; i++) {
             result[(int)i] = ThreadLocalRandom.current().nextLong(this.min, this.max);
         }
-
         return result;
     }
 
@@ -44,10 +38,7 @@ public class LongGenerator implements Iterable<Long>, Iterator<Long>, Generator 
     public List<Long> generateAsList(int size) {
         if (size < 0)
             throw new IllegalArgumentException("Size should not be <= 0");
-
         List<Long> resultList = new ArrayList<Long>();
-        Random rand = new Random();
-
         for (int i = 0; i < size; i++) {
             resultList.add(ThreadLocalRandom.current().nextLong(this.min, this.max));
         }
@@ -55,11 +46,9 @@ public class LongGenerator implements Iterable<Long>, Iterator<Long>, Generator 
     }
 
     public Iterator<Long> iterator(int size) {
-
         // Initializing Iterator variables
         this.size = size;
         this.index = 0;
-        this.rand = new Random();
         return this;
     }
 
@@ -72,7 +61,7 @@ public class LongGenerator implements Iterable<Long>, Iterator<Long>, Generator 
     @Override
     public Long next() {
         this.index++;
-        return this.rand.nextLong() * ((this.max + 1 - this.min) + this.min);
+        return ThreadLocalRandom.current().nextLong(this.min, this.max);
     }
 
     @Override
