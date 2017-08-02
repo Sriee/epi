86d064ecaf2a4ed1875fc7771aa9f1bbfdac9945
Sriee <psg.sriee@gmail.com>
2017-08-01 23:47:18 -0500

    Comparator for List/Set/Map implemented methods

---
+++ Interview/src/util/Compare.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.util.List;
+import java.util.Set;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class Compare {
+
+	public static <T> boolean compare(List<T> first, List<T> second){
+		if(first == null) return false;
+		if(second == null) return false;
+		
+		if(first.size() != second.size()) return false;
+		
+		for(T item : first){
+			if(!second.contains(item)) return false;
+		}
+		return true;
+	}
+
+	public static <S> boolean compare(Set<S> first, Set<S> second){
+		if(first == null) return false;
+		if(second == null) return false;
+		if(first.size() != second.size()) return false;
+		return first.containsAll(second);
+	}
+	
+	public static <K, V> boolean compare(Map<K, V> first, Map<K, V> second){
+		if(first == null) return false;
+		if(second == null) return false;
+		if(first.size() != second.size()) return false;
+		
+		if(!first.keySet().containsAll(second.keySet()))
+			return false;
+		
+		Set<Map.Entry<K, V>> entrySet = first.entrySet();
+		for(Entry<K, V> entry: entrySet){
+			K key = entry.getKey();
+			
+			if(!second.containsKey(key)) return false;
+			
+			if(entry.getValue() != second.get(key)) return false;
+		}
+		
+		return true;
+	}
+}
