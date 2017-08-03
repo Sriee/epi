373aaadf1202073fb4f3d6f66b3e69b9bdc4e4df
sriee <sriee.ammapet@spirent.com>
2017-08-03 17:47:30 -0500

    Test Suite for checking the ArrayUtil features implemented so far

---
+++ Interview/test/util/ArrayUtilTestSuite.java
@@ -0,0 +1,23 @@
+package util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+		ArrayUtilCloneTest.class,
+		ArrayUtilContainsTest.class,
+		ArrayUtilEmptyTest.class,
+		ArrayUtilIndexOfTest.class,
+		ArrayUtilNotEmptyTest.class,
+		ArrayUtilNullEmptyTest.class,
+		ArrayUtilReverseTest.class,
+		ArrayUtilSubarrayTest.class,
+		ArrayUtilToObjectAndTypeTest.class,
+		ArrayUtilToPrimitiveTest.class,
+})
+
+public class ArrayUtilTestSuite {
+
+}
