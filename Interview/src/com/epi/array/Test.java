c262bff7305357d26d57e3ba845c39ec3b255842
Sriee <psg.sriee@gmail.com>
2017-08-02 22:37:19 -0500

    Test file

--- Interview/src/com/epi/array/Test.java
+++ Interview/src/com/epi/array/Test.java
@@ -2,25 +2,14 @@ package com.epi.array;
 
 import java.util.Arrays;
 
-public class Test {
+import util.generator.DoubleGenerator;
 
-	private static void print(int arr[]){
-		System.out.println(Arrays.toString(arr));
-	}
-	
-	private static void print(double arr[]){
-		System.out.println(Arrays.toString(arr));
-	}
+public class Test {
 	
 	public static void main(String[] args) {
-		int[] intArr = {1, 2, 3, 4};
-		double[] doubleArr = {2.4, 4.654, 3.564};
-		
-		print(intArr);
-		print(doubleArr);
-		
-//		for(int i = 0; i < 5; i++)
-//			System.out.print(dummy[i] + " ");
+		DoubleGenerator fg = new DoubleGenerator(-4526, 8235, 4);
+		double[] test = fg.generate(15);
+		System.out.println(Arrays.toString(test));
 	}
 
 }
