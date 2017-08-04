67a4fda7de04e4af4ed48a80eb32ff7391f4a3c4
Sriee <psg.sriee@gmail.com>
2017-08-03 19:49:48 -0500

    Added Append unit test to Test suite

--- Interview/test/util/ArrayUtilTestSuite.java
+++ Interview/test/util/ArrayUtilTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
+		ArrayUtilAppendTest.class,
 		ArrayUtilCloneTest.class,
 		ArrayUtilContainsTest.class,
 		ArrayUtilEmptyTest.class,
