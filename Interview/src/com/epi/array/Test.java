00ba6445bc5bb033822caadc2bf4cf8a364a9975
Sriee <psg.sriee@gmail.com>
2017-08-03 23:11:03 -0500

    Insert element implementation

--- Interview/src/com/epi/array/Test.java
+++ Interview/src/com/epi/array/Test.java
@@ -2,14 +2,32 @@ package com.epi.array;
 
 import java.util.Arrays;
 
-import util.generator.DoubleGenerator;
-
 public class Test {
 	
+	private static void how(int... varargs){
+		for(int i: varargs)
+			System.out.print(i + " ");
+		
+		System.out.println();
+	}		
+	
 	public static void main(String[] args) {
-		DoubleGenerator fg = new DoubleGenerator(-4526, 8235, 4);
-		double[] test = fg.generate(15);
-		System.out.println(Arrays.toString(test));
+		int[] arr = {1,2,4,5};
+		int[] inserted = new int[arr.length + 1];
+		int pos = 4;
+		
+		if(pos > arr.length || pos < 0)
+			throw new ArrayIndexOutOfBoundsException();
+		
+		System.arraycopy(arr, 0, inserted, 0, pos);
+		inserted[pos] = 3;
+		System.arraycopy(arr, pos, inserted, pos + 1, arr.length - pos);
+		
+		System.out.println(Arrays.toString(inserted));
+		
+		how(1, 2, 3);
+		how(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		how(1, 2, 3, 4);
+		how(1, 2, 3, -2);
 	}
-
 }
