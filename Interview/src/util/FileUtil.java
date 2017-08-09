c074ea4c58181e1b8bdad9bf4fefb7723f30f5f2
sriee <sriee.ammapet@spirent.com>
2017-08-09 13:56:53 -0500

    head command functionality

--- Interview/src/util/FileUtil.java
+++ Interview/src/util/FileUtil.java
@@ -1,69 +1,162 @@
 package util;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtil {
 
-    private String file_name = null;
-    private long length = -1;
+    private String fileName;
+    private long headLength;
+    private long tailLength;
+    private long sleep;
 
     public FileUtil(){
-        this.length = 5;
+        this(null, 10, 10, 1);
     }
 
-    public FileUtil(String fileName, long length){
-        this.length = length;
-        this.file_name = fileName;
+    public FileUtil(String fileName){
+        this(fileName, 10, 10, 1);
+    }
+
+    public FileUtil(String fileName, long headLength, long tailLength, long sleep){
+        this.fileName = fileName;
+        this.headLength = headLength;
+        this.tailLength = tailLength;
+        this.sleep = sleep;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getHeadLength() {
+        return headLength;
+    }
+
+    public void setHeadLength(long headLength) {
+        this.headLength = headLength;
+    }
+
+    public long getTailLength() {
+        return tailLength;
+    }
+
+    public void setTailLength(long tailLength) {
+        this.tailLength = tailLength;
+    }
+
+    public long getSleep() {
+        return sleep;
+    }
+
+    public void setSleep(long sleep) {
+        this.sleep = sleep;
+    }
+
+    /******************* HEAD *******************************/
+
+    /**
+     * Displays the first [count] lines of the specified file.  If count is omitted it defaults to 10.
+     */
+    public void head(){ headFunctionality(); }
+
+    /**
+     * Displays the first [count] lines of the specified file.  If count is omitted it defaults to 10.
+     *
+     * @param fileName Name of the file
+     */
+    public void head(String fileName){
+        this.setFileName(fileName);
+        headFunctionality();
+    }
+
+    /**
+     * Displays the first [count] lines of the specified file.  If count is omitted it defaults to 10.
+     *
+     * @param headLength number of lines to display
+     */
+    public void head(long headLength){
+        this.setHeadLength(headLength);
+        headFunctionality();
+    }
+
+    /**
+     * Displays the first [count] lines of the specified file.  If count is omitted it defaults to 10.
+     *
+     * @param fileName Name of the file
+     * @param headLength number of lines to display
+     */
+    public void head(String fileName, long headLength){
+        this.setFileName(fileName);
+        this.setHeadLength(headLength);
+        headFunctionality();
+    }
+
+    /**
+     * 'head' command feature implementation
+     */
+    private void headFunctionality(){
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(this.getFileName()));
+            String currentLine;
+            long linesRead = 1;
+            while ((currentLine = bufferedReader.readLine()) != null && linesRead <= this.getHeadLength()) {
+                System.out.println(currentLine);
+                linesRead += 1;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) bufferedReader.close();
+                if (fileReader != null) fileReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /******************* TAIL *******************************/
+    public void tail(){
+        tailFunctionality();
     }
 
     public void tail(String fileName){
-    	RandomAccessFile raf = null;
-    	List<String> lines = new ArrayList<String>();
-
-        StringBuilder builder = new StringBuilder();
-
-        try {
-            raf = new RandomAccessFile(fileName, "r");
-            length = raf.length(); // Length of the file in bytes
-            length--;
-            
-            for(long seek = length-1; seek >= 0; --seek){
-                raf.seek(seek);
-                char ch = (char)raf.read();
-                builder.insert(0, ch);
-                if(ch == '\n'){ // Encountered new line
-                    lines.add(0, builder.toString());
-                    builder = new StringBuilder();
-
-                    if(lines.size() == 5) break;
-                }
-            }
-            for(String line : lines)
-                System.out.print(line);
-
-        } catch (IOException e){
-            e.printStackTrace();
-        } finally {
-            try {
-                if (raf != null) raf.close();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        }    	
+        this.setFileName(fileName);
+        tailFunctionality();
     }
 
-    public void tail(String fileName, long length){
-    	RandomAccessFile raf = null;
-    	List<String> lines = new ArrayList<String>();
+    public void tail(long tailLength){
+        this.setTailLength(tailLength);
+        tailFunctionality();
+    }
 
+    public void tail(String fileName, long tailLength){
+        this.setFileName(fileName);
+        this.setTailLength(tailLength);
+        tailFunctionality();
+    }
+
+    private void tailFunctionality(){
+        RandomAccessFile raf = null;
+        List<String> lines = new ArrayList<String>();
         StringBuilder builder = new StringBuilder();
 
         try {
-            raf = new RandomAccessFile(this.file_name, "r");
-            length = raf.length(); // Length of the file in bytes
+            raf = new RandomAccessFile(this.getFileName(), "r");
+            long length = raf.length(); // Length of the file in bytes
 
             for(long seek = length-1; seek >= 0; --seek){
                 raf.seek(seek);
@@ -73,7 +166,7 @@ public class FileUtil {
                     lines.add(0, builder.toString());
                     builder = new StringBuilder();
 
-                    if(lines.size() == this.length) break;
+                    if(lines.size() == this.getTailLength()) break;
                 }
             }
             for(String line : lines)
@@ -87,12 +180,13 @@ public class FileUtil {
             } catch (IOException e){
                 e.printStackTrace();
             }
-        }    	
+        }
     }
 
     public static void main(String[] args) {
-    	FileUtil fu = new FileUtil();
-    	System.out.println("Starting...");
-    	fu.tail("/home/sriee/Git/epi/Interview/src/util/data/harley.txt");
+    	FileUtil fu = new FileUtil("/Users/sriee/epi/Interview/src/util/data/harley.txt");
+        System.out.println("Starting...");
+    	fu.head();
+        System.out.println("(END)...");
     }
 }
