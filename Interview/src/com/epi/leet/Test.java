f12b8ef7614133630ee7f31b4728d2f18630a200
sriee <sriee.ammapet@spirent.com>
2017-09-11 10:20:33 -0500

    Misc

--- Interview/src/com/epi/leet/Test.java
+++ Interview/src/com/epi/leet/Test.java
@@ -4,41 +4,11 @@ import java.util.*;
 
 public class Test {
 
-	private static Set<Character> stringToCharSet(String inp){
-		Set<Character> load = new HashSet<Character>();
-		inp = inp.toUpperCase();
-		for(int i = 0; i < inp.length(); i++) load.add(inp.charAt(i));
-		return load;
-	}
-	
-	private static String[] findWords(String[] words) {
-		Set<Character> top = stringToCharSet("QWERTYUIOP");
-		Set<Character> middle = stringToCharSet("ASDFGHJKL");
-		Set<Character> bottom = stringToCharSet("ZXCVBNM");
-		
-		List<String> result = new ArrayList<String>();
-		Set<Character> dest = null;
-		
-		for(int index = 0; index < words.length; index++) {
-			dest = stringToCharSet(words[index]);
-			if(top.containsAll(dest)) {
-				result.add(words[index]);
-			} else if(middle.containsAll(dest)) {
-				result.add(words[index]);
-			} else if(bottom.containsAll(dest)) {
-				result.add(words[index]);
-			}
-		}
-		
-		return result.toArray(new String[0]);
-	}
-	
 	public static void main(String[] args) {
-		String[] words= {"asdfghjkl", "dad", "was"};
-		String[] results = findWords(words);
-		
-		for(int i = 0; i < results.length; i++)
-			System.out.println(results[i]);
-	}
+		System.out.println('a' - '0');
+        System.out.println('z' - '0');
+        System.out.println('A' - '0');
+        System.out.println('Z' - '0');
+    }
 
 }
