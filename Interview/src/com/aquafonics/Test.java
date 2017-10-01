246da99d996e447ef05ab023efebff7a0bd1eb4c
Sriee <psg.sriee@gmail.com>
2017-10-01 14:10:49 -0500

    Changes for new key

--- Interview/src/com/aquafonics/Test.java
+++ Interview/src/com/aquafonics/Test.java
@@ -7,35 +7,62 @@ import java.util.LinkedList;
 public class Test {
 	private HashMap<Rule, IntervalTree> map = null;
 	private LinkedList<String> conflictList = null;
-	
+	private LinkedList<String> warningList = null;
+			
 	public Test() {
 		this.map = new HashMap<>();
 		this.conflictList = new LinkedList<>();
+		this.warningList = new LinkedList<>();
 	}
 
-	private void add(String action, String actuator, int thresholdValue, String operator, String sensorId, String ruleId,
-			String actuatorId) {
+	/**
+	 * TODO: Add call to check with opposite actions
+	 * @param action
+	 * @param actuator
+	 * @param thresholdValue
+	 * @param operator
+	 * @param ruleId
+	 * @param sensorId
+	 */
+	private void add(String action, String actuator, int thresholdValue, String operator, String ruleId, String sensorId) {
 		int min = 0, max = 150;
 		Rule rule = null;
 		IntervalTree it = null;
+		String oppositeAction = getOppositeAction(action);
+		
+		if(oppositeAction == null) {
+			System.out.println("Received 'null' for " + action);
+		}
 		try {
-				rule = new Rule(sensorId, actuatorId);
-				it = (this.map.containsKey(rule)) ? this.map.get(rule) : new IntervalTree();
+			rule = new Rule(action, actuator);
+			it = (this.map.containsKey(rule)) ? this.map.get(rule) : new IntervalTree();
 			switch (operator) {
 			case "<":
-				it.put(min, thresholdValue - 1, ruleId);
+				it.put(min, thresholdValue - 1, ruleId, sensorId);
 				this.map.put(rule, it);
+				
+				// Check for the presence of opposite action
+				rule = new Rule(oppositeAction, actuator);
+				
+				if(oppositeAction != null) {
+					if(this.map.containsKey(rule)) {
+						this.check(thresholdValue, max, oppositeAction, ruleId, sensorId);	
+					} else {
+						this.add(oppositeAction, actuator, thresholdValue, ">=", ruleId, sensorId);
+					}
+				} 
 				break;
 			case "<=":
-				it.put(min, thresholdValue, ruleId);
+				it.put(min, thresholdValue, ruleId, sensorId);
 				this.map.put(rule, it);
+				
 				break;
 			case ">":
-				it.put(thresholdValue + 1, max, ruleId);
+				it.put(thresholdValue + 1, max, ruleId, sensorId);
 				this.map.put(rule, it);
 				break;
 			case ">=":
-				it.put(thresholdValue, max, ruleId);
+				it.put(thresholdValue, max, ruleId, sensorId);
 				this.map.put(rule, it);
 				break;
 			case "=":
@@ -43,11 +70,32 @@ public class Test {
 			case "!=":
 				break;
 			}
-		} catch (RuleConflictException r) {
-			this.conflictList.add(ruleId + " in conflict with " + r.getMessage());
+		} catch (RuleWarnings r) {
+			this.warningList.add("WARNING: " + sensorId + " in conflict with" + r.getMessage());
 		}
 	}
 
+	private void check(int min, int max, String action, String ruleId, String sensorId) {
+		/*try {
+			
+		} catch (RuleConflictException e) {
+			this.conflictList.add(ruleId + " in conflict with " + e.getMessage());
+		}*/
+	}
+	
+	private String getOppositeAction(String action) {
+		String oppositeAction = null;
+		switch(action) {
+		case "turn on" : 
+			oppositeAction = "turn off";
+			break;
+		case "turn off":
+			oppositeAction = "turn on";
+			break;
+		}
+		return oppositeAction;
+	}
+	
 	public static void main(String[] args) {
 		Connection connection = null;
 		Test t = new Test();
@@ -66,14 +114,12 @@ public class Test {
 			ResultSet rs = stmt.executeQuery();
 
 			while (rs.next()) {
-				/* t.add(Integer.parseInt(rs.getString("threshold_value")), rs.getString("operator_name"), 
-						rs.getString("sensor_id"), null, rs.getString("rule_rid")); */
-				System.out.println(rs.getString("Action_Name") + " " + rs.getString("Action_Parameters") + " "
-						+ Integer.parseInt(rs.getString("threshold_value")) + " " + rs.getString("operator_name") + " "
-						+ rs.getString("rule_rid") + " " + rs.getString("sensor_id"));
+				t.add(rs.getString("Action_Name"), rs.getString("Action_Parameters"), 
+						Integer.parseInt(rs.getString("threshold_value")), rs.getString("operator_name"),
+						rs.getString("rule_rid"), rs.getString("sensor_id"));
 			}
 			
-			/* Print conflicts 
+			// Print conflicts 
 			if(t.conflictList.isEmpty()) {
 				System.out.println("Rules have no conflict.");
 			} else {
@@ -81,8 +127,7 @@ public class Test {
 				for(String item : t.conflictList) {
 					System.out.println(item);
 				}
-			} */
-			
+			} 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -94,8 +139,6 @@ public class Test {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-
 		}
 	}
-
 }
