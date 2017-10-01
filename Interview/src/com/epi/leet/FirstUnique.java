c8158996fd702ab9fbcb7414b6ab265cc1776709
Sriee <psg.sriee@gmail.com>
2017-10-01 18:25:57 -0500

    finding unique character in a string

--- Interview/src/com/epi/leet/FirstUnique.java
+++ Interview/src/com/epi/leet/FirstUnique.java
@@ -1,18 +1,44 @@
 package com.epi.leet;
 
 import java.util.Map;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class FirstUnique{
 	
-	public static void main(String[] args){
-		String inp = "leetcode";
-		Map<String, Integer> hm = new HashMap<>();
-
+	private static int checkTreeMap(String inp) {
 		int idx = -1;
+		if(inp == null) return idx;
 		
-		for(int i = 0
-		if(idx == -1) System.out.println(0);
-		else System.out.println(idx);
+		inp = inp.trim();
+		
+		if(inp.isEmpty()) return idx;
+		
+		Map<Character, Integer> tm = new LinkedHashMap<>();
+		for(int i = 0; i < inp.length(); i++) {
+			char temp = inp.charAt(i);
+			if(tm.containsKey(temp)) {
+				tm.put(temp, tm.get(temp) + 1);
+			} else {
+				tm.put(temp, 1);
+			}
+		}
+		
+		for(Character ch : tm.keySet()) {
+			if(tm.get(ch) > 1) continue;
+			
+			idx = inp.indexOf(ch);
+			break;
+		}
+	
+		return idx;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(checkTreeMap("leetcode"));
+		System.out.println(checkTreeMap("   "));
+		System.out.println(checkTreeMap("aaaaa"));
+		System.out.println(checkTreeMap("lahcdl"));
+		System.out.println(checkTreeMap(" hasdgfahs"));
+		System.out.println(checkTreeMap(null));
 	}
 }
