1e7c86acd36aada2cdfd9fee686a6e21e158caf3
Sriee <psg.sriee@gmail.com>
2017-10-01 14:11:16 -0500

    test set

--- Interview/src/com/aquafonics/Scratch.java
+++ Interview/src/com/aquafonics/Scratch.java
@@ -1,39 +1,17 @@
 package com.aquafonics;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class Scratch {
-
-	private static boolean isOverride(int[] arr1, int[] arr2) {
-		
-		if(arr1[0] == arr2[0] && (arr1[1] < arr2[1] || arr2[1] < arr1[1])) {
-			return true;
-		} else if(arr1[1] == arr2[1] && (arr1[0] < arr2[0] || arr2[0] < arr1[0])){
-			return true;
-		}
-		return false;
-	}
 	
 	public static void main(String[] args) {
-		Rule r1 = new Rule("humidity1", "actuator1");
-		Rule r2 = new Rule("humidity1", "actuator2");
-		Rule r3 = new Rule("humidity1", "actuator1");
-		Rule r4 = new Rule("humidity2", "actuator2");
-		
-		System.out.println(r1);
-		System.out.println(r1.hashCode() + " " + r2.hashCode() + " " + r3.hashCode() + " " + r4.hashCode());
-		Map<Rule, Integer> hm = new HashMap<>();
-		
-		hm.put(r1, 1);
-		hm.put(r2, 2);
-		hm.put(r4, 4);
-		if(hm.containsKey(r3))
-			System.out.println("Done deal.");
-
-		System.out.println(isOverride(new int[]{1, 10}, new int[]{1, 5}));
-		System.out.println(isOverride(new int[]{1, 10}, new int[]{1, 10}));
-		System.out.println(isOverride(new int[]{1, 10}, new int[]{2, 5}));
+		Set<String> s = new HashSet<>();
+		List<String> l = new ArrayList<>();
+		l.add("221"); l.add("223"); l.add("224");
+		System.out.println(l.contains("221"));
+		l.add("221");
+		s.addAll(l);
+		System.out.println(s.toString());
 	}
 
 }
