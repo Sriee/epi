c63ed85a108c84e1f0ad450f3bcba53215d7300f
sriee <sriee.ammapet@spirent.com>
2017-10-13 10:17:12 -0500

    String to hex

---
+++ Interview/src/com/epi/array/Base.java
@@ -0,0 +1,28 @@
+package com.epi.array;
+
+public class Base{
+	public static void main(String[] args){
+        long temp2 =0;
+        long temp = Integer.toUnsignedLong(-1);
+	    if (temp < 0) {
+            temp = temp * -1;
+            temp = temp ^ 2147483647;
+            temp = temp + 1;
+        }
+        System.out.println(temp);
+		long num = temp;
+		char[] ch = {'a', 'b', 'c', 'd', 'e', 'f'};
+		boolean isNeg = num < 0;
+        StringBuilder sb = new StringBuilder();
+		while(num != 0){
+			int r = (int)num % 16;
+			if(r > 9)
+				sb.append(ch[r - 10]);
+			else
+				sb.append(r);
+			num /= 16;
+        }
+
+        System.out.println(sb.reverse().toString());
+	}
+}
