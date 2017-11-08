46ee71653f94cdd15a35381ab79270ff21a4c5ea
Sriee <psg.sriee@gmail.com>
2017-11-07 20:34:59 -0600

    Calculate points question

--- Interview/src/com/epi/leet/Flip.java
+++ Interview/src/com/epi/leet/Flip.java
@@ -1,5 +1,7 @@
 package com.epi.leet;
 
+import java.util.Stack;
+
 public class Flip {
 
     public static int flipBit(int number){
@@ -19,6 +21,32 @@ public class Flip {
         return (mask ^ number);
     }
 
+    public static int calPoints(String[] ops) {
+        int sum = 0, num, b;
+        if(ops == null || ops.length == 0) return sum;
+        Stack<Integer> stack = new Stack<>();
+        for(String token : ops){
+            if(token.equals("C")){
+                sum -= stack.pop();
+            } else if(token.equals("D")){
+                num = 2 * stack.peek();
+                stack.push(num);
+                sum += num;
+            } else if(token.equals("+")){
+                b = stack.pop();
+                num = b + stack.peek();
+                sum += num;
+                stack.push(b);
+                stack.push(num);
+            } else {
+                num = Integer.parseInt(token);
+                sum += num;
+                stack.push(num);
+            }
+        }
+        return sum;
+    }
+    
     /**
      * Decimal to Hexadecimal conversion 
      * 
@@ -29,6 +57,7 @@ public class Flip {
     private static void decimalToHex(int num) {
     	StringBuilder sb = new StringBuilder();
     	char literals[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    	
     	while(num != 0) {
     		sb.append(literals[num & 15]);
     		num = num >>> 4;
@@ -36,7 +65,10 @@ public class Flip {
     	System.out.println(sb.reverse().toString());
     }
     public static void main(String[] args) {
-        System.out.println(flipBit(2147483647));
-        decimalToHex(-2);
+    	System.out.println(calPoints(new String[] {"5","2","C","D","+"}));
+    	System.out.println(calPoints(new String[] {"5","-2","4","C","D","9","+","+"}));
+    	System.out.println(calPoints(new String[] {"5"}));
+        /*System.out.println(flipBit(2147483647));
+        decimalToHex(-2);*/
     }
 }
