7803fc44ddfe5099eb97a85df909a21c3ce6a39c
Sriee <psg.sriee@gmail.com>
2017-11-12 23:23:19 -0600

    Zig zag order traversal. Not yet complete

--- Interview/src/com/epi/tree/Traversal.java
+++ Interview/src/com/epi/tree/Traversal.java
@@ -5,7 +5,44 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Traversal {
-
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> result = new LinkedList<>();
+        List<Integer> innerList = new LinkedList<>();
+        
+        int current = 1, next = 0, level = 0; 
+        queue.add(root);
+        while(!queue.isEmpty()){
+            
+            TreeNode node = queue.remove();
+            current--;
+            if(node != null){
+                innerList.add(node.val);
+                
+                if(level % 2 == 0){
+                    queue.add(node.right);
+                    queue.add(node.left);
+                } else {
+                    queue.add(node.left);
+                    queue.add(node.right);    
+                }
+                
+                next += 2;
+            }
+            
+            if(current == 0){
+                if(!innerList.isEmpty()){
+                    result.add(innerList);
+                }
+                innerList = new LinkedList<>();
+                current = next;
+                next = 0;
+                level++;
+            }
+        }
+        return result;
+	}
+	
     public List<List<Integer>> levelOrder(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> result = new LinkedList<>();
