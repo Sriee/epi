8ef9659a6e13a2c9902671e87f3448c624fdc37f
sriee <sriee.ammapet@spirent.com>
2017-11-27 17:17:03 -0600

    Path parameter change

--- RuleConflict/src/com/json/Driver.java
+++ RuleConflict/src/com/json/Driver.java
@@ -3,6 +3,7 @@ package com.json;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import com.google.gson.Gson;
@@ -12,30 +13,31 @@ import com.logger.FileLogger;
 
 public class Driver {
 
-	public static void main(String[] args) throws IOException {
-		// Configure gson
-	    GsonBuilder gsonBuilder = new GsonBuilder();
-	    gsonBuilder.registerTypeAdapter(Input.class, new InputDeserializer());
-	    Gson gson = gsonBuilder.create();
-	    
-	    FileLogger log = new FileLogger();
-	    
-	    JsonReader reader = null;
-		try {
-			reader = new JsonReader(new FileReader("/home/sriee/Git/epi/RuleConflict/resources/boolean_expression.json"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	    Input sample = gson.fromJson(reader, Input.class);
-	    log.writeLog(sample.toString());
-	    
-	    if(sample.getLiterals() != null){
-	    	for(Map.Entry<String, Literals> entry : sample.getLiterals().entrySet()){
-	    		log.writeLog(entry.getKey() + "\t" + entry.getValue().toString());
-	    	}
-	    } else {
-	    	System.out.println("Null");
-	    }
-	}
+    public static void main(String[] args) throws IOException {
+        // Configure gson
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(Input.class, new InputDeserializer());
+        Gson gson = gsonBuilder.create();
+
+        FileLogger log = FileLogger.instance();
+
+        JsonReader reader = null;
+        try {
+            reader = new JsonReader(new FileReader(Paths.get(Paths.get("..").toAbsolutePath().toString(),
+                    "resources/boolean_expression.json").normalize().toString()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Input sample = gson.fromJson(reader, Input.class);
+        log.writeLog(sample.toString());
+
+        if (sample.getLiterals() != null) {
+            for (Map.Entry<String, Literals> entry : sample.getLiterals().entrySet()) {
+                log.writeLog(entry.getKey() + "\t" + entry.getValue().toString());
+            }
+        } else {
+            System.out.println("Null");
+        }
+    }
 
 }
