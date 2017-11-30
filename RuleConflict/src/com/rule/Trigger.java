42ce73d3358c7dade65df44d518bb7e06122c183
Sriee <psg.sriee@gmail.com>
2017-11-29 23:13:11 -0600

    stub util code

--- RuleConflict/src/com/rule/Trigger.java
+++ RuleConflict/src/com/rule/Trigger.java
@@ -4,7 +4,58 @@ import com.json.LogicalOperator;
 
 public class Trigger {
 	int triggerId;
+	String name;
 	LogicalOperator operator;
 	int value;
 	int priority;
+	
+	public Trigger() { this(-1, null, null, -1, -1); }
+
+	/**
+	 * @param triggerId
+	 * @param operator
+	 * @param value
+	 * @param priority
+	 */
+	public Trigger(int triggerId, String name, LogicalOperator operator, int value, int priority) {
+		this.triggerId = triggerId;
+		this.name = name;
+		this.operator = operator;
+		this.value = value;
+		this.priority = priority;
+	}
+
+	
+	/**
+	 * @param triggerId
+	 * @param name
+	 * @param operator
+	 * @param value
+	 */
+	public Trigger(int triggerId, String name, LogicalOperator operator, int value) {
+		this(triggerId, name, operator, value, -1);
+	}
+
+	/**
+	 * @param triggerId
+	 * @param operator
+	 * @param value
+	 */
+	public Trigger(int triggerId, LogicalOperator operator, int value) {
+		this(triggerId, null, operator, value, -1);
+	}
+
+	public String toExpressionString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("(");
+		sb.append(this.name + " " + this.operator.toString() + " " + this.value);
+		sb.append(")");
+		return sb.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Trigger [triggerId=" + triggerId + ", name=" + name + ", operator=" + operator + ", value=" + value
+				+ ", priority=" + priority + "]";
+	}	
 }
