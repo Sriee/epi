679f8191d7baf22989e85585b22ce5920eceb915
Sriee <psg.sriee@gmail.com>
2017-11-30 01:04:03 -0600

    Change exception handler place to local

--- RuleConflict/src/com/logger/FileLogger.java
+++ RuleConflict/src/com/logger/FileLogger.java
@@ -16,43 +16,44 @@ public class FileLogger implements Logger {
 
 	private String fileName = "rule.log"; 
 	private File logFile = null;
-    public static FileLogger fileLoggerObj = null;
+	public static FileLogger fileLoggerObj = null;
 
-	private FileLogger() throws IOException {
+	private FileLogger() {
 		super();
 		Path currentDir = Paths.get(".");
 		this.fileName = currentDir.toString() + "/" + this.fileName;
-		
+
 		this.logFile = new File(this.fileName);
-		if( !this.logFile.exists() ){
-			if( ! logFile.createNewFile() )
-				throw new NullPointerException(fileName + " file not created.");
-		} else {
-			BufferedWriter bw = null;
-			try {
+		try {
+			if( !this.logFile.exists() ){
+				if( ! logFile.createNewFile() )
+					throw new NullPointerException(fileName + " file not created.");
+			} else {
+				BufferedWriter bw = null;
+
 				bw = new BufferedWriter( new FileWriter(this.logFile.getCanonicalPath(), true));
 				bw.write("\n[=============================================New Run=============================================]\n");
-				bw.close();
-			} catch(FileNotFoundException e){
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
+				bw.close();	
+			}
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
-	public static FileLogger instance() throws IOException{
-        if(fileLoggerObj == null){
-            fileLoggerObj = new FileLogger();
-        }
-        return fileLoggerObj;
-    }
+	public static FileLogger instance() {
+		if(fileLoggerObj == null){
+			fileLoggerObj = new FileLogger();
+		}
+		return fileLoggerObj;
+	}
 
 	@Override
 	public void writeLog(String entry){
 		if( logFile == null )
 			throw new NullPointerException("Log file not initialized.");
-		
+
 		BufferedWriter bw = null;
 		try {
 			bw = new BufferedWriter( new FileWriter(this.logFile.getCanonicalPath(), true));
@@ -64,19 +65,19 @@ public class FileLogger implements Logger {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void writeLog(Collection<String> entry){
 		if( logFile == null )
 			throw new NullPointerException("Log file not initialized.");
 		Iterator<String> iter = entry.iterator();
-	
+
 		BufferedWriter bw = null;
 		try {
 			bw = new BufferedWriter( new FileWriter(this.logFile.getCanonicalPath(), true));
 			while( iter.hasNext() )
 				bw.write(this.getTimeStamp() + iter.next() + "\n");
-			
+
 			bw.close();
 		} catch(FileNotFoundException e){
 			e.printStackTrace();
@@ -84,7 +85,7 @@ public class FileLogger implements Logger {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private String getTimeStamp(){
 		String timeStamp = "";
 		SimpleDateFormat dateFormat = new SimpleDateFormat("[M-dd-yyyy H:mm] ");
