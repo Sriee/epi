36251854460dc268682b22b93621f98e8d8d8823
Sriee <psg.sriee@gmail.com>
2017-11-30 23:04:58 -0600

    POJOs for hibernate

---
+++ RuleConflict/src/com/entity/Action.java
@@ -0,0 +1,167 @@
+package com.entity;
+
+import javax.persistence.*;
+
+import com.json.LogicalOperator;
+
+@Entity
+@Table(name = "Action")
+public class Action {
+	
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "actuator_id")
+	private int actuatorId;
+	
+	@Column(name = "operator")
+	private String operator; 
+	
+	@Column(name = "value")
+	private int value;
+	
+	@Column(name = "priority")
+	private int priority;
+	
+	public Action() {}
+	/**
+	 * @param name
+	 * @param actuatorId
+	 * @param operator
+	 * @param value
+	 */
+	public Action(String name, int actuatorId, LogicalOperator operator, int value) {
+		this(null, name, actuatorId, operator, value, -1);
+	}
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param actuatorId
+	 * @param operator
+	 * @param value
+	 */
+	public Action(Long id, String name, int actuatorId, LogicalOperator operator, int value) {
+		this(id, name, actuatorId, operator, value, -1);
+	}
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param actuatorId
+	 * @param operator
+	 * @param value
+	 * @param priority
+	 */
+	public Action(Long id, String name, int actuatorId, LogicalOperator operator, int value, int priority) {
+		this.id = id;
+		this.name = name;
+		this.actuatorId = actuatorId;
+		this.operator = operator.toString();
+		this.value = value;
+		this.priority = priority;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the actuatorId
+	 */
+	public int getActuatorId() {
+		return actuatorId;
+	}
+
+	/**
+	 * @param actuatorId the actuatorId to set
+	 */
+	public void setActuatorId(int actuatorId) {
+		this.actuatorId = actuatorId;
+	}
+
+	/**
+	 * @return the operator
+	 */
+	public String getOperator() {
+		return operator;
+	}
+
+	/**
+	 * @param operator the operator to set
+	 */
+	public void setOperator(LogicalOperator operator) {
+		this.operator = operator.toString();
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the priority
+	 */
+	public int getPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	public String toExpressionString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("(");
+		sb.append(this.name + " " + this.operator.toString() + " " + this.value);
+		sb.append(")");
+		return sb.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Action [id=" + id + ", name=" + name + ", actuatorId=" + actuatorId + ", operator=" + operator
+				+ ", value=" + value + ", priority=" + priority + "]";
+	}	
+}
---
+++ RuleConflict/src/com/entity/Trigger.java
@@ -0,0 +1,186 @@
+package com.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.json.LogicalOperator;
+
+@Entity
+@Table(name = "Triggers")
+public class Trigger {
+
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "sensor_id")
+	private int sensorId;
+	
+	@Column(name = "operator")
+	private String operator;
+	
+	@Column(name = "value")
+	private int value;
+	
+	@Column(name = "priority")
+	private int priority;
+	
+	public Trigger(){ 
+		this.id = null;
+		this.name = null;
+		this.sensorId = -1;
+		this.operator = null;
+		this.value = -1;
+		this.priority = -1;
+	} 
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param sensorId
+	 * @param operator
+	 * @param value
+	 * @param priority
+	 */
+	public Trigger(Long id, String name, int sensorId, String operator, int value, int priority){
+		this.id = id;
+		this.name = name;
+		this.sensorId = sensorId;
+		this.operator = operator;
+		this.value = value;
+		this.priority = priority;
+	}
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param sensorId
+	 * @param operator -> Logical Operator
+	 * @param value
+	 * @param priority
+	 */
+	public Trigger(Long id, String name, int sensorId, LogicalOperator operator, int value, int priority){
+		this(id, name, sensorId, operator.toString(), value, priority);
+	}
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param sensorId
+	 * @param operator
+	 * @param value
+	 */
+	public Trigger(Long id, String name, int sensorId, String operator, int value){
+		this(id, name, sensorId, operator.toString(), value, -1);
+	}
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param sensorId
+	 * @param operator -> Logical Operator
+	 * @param value
+	 * @param priority
+	 */
+	public Trigger(Long id, String name, int sensorId, LogicalOperator operator, int value){
+		this(id, name, sensorId, operator.toString(), value, -1);
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the sensorId
+	 */
+	public int getSensorId() {
+		return sensorId;
+	}
+
+	/**
+	 * @param sensorId the sensorId to set
+	 */
+	public void setSensorId(int sensorId) {
+		this.sensorId = sensorId;
+	}
+
+	/**
+	 * @return the operator
+	 */
+	public String getOperator() {
+		return operator;
+	}
+
+	/**
+	 * @param operator the operator to set
+	 */
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the priority
+	 */
+	public int getPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority to set
+	 */
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	@Override
+	public String toString() {
+		return "Trigger [id=" + id + ", name=" + name + ", sensorId=" + sensorId + ", operator=" + operator + ", value="
+				+ value + ", priority=" + priority + "]";
+	}	
+}
