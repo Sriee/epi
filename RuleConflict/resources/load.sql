1630801fe0031ce16612b7b24681639c0ff188bc
Sriee <psg.sriee@gmail.com>
2017-12-02 16:09:36 -0600

    Schema change

--- RuleConflict/resources/load.sql
+++ RuleConflict/resources/load.sql
@@ -1,25 +1,25 @@
 USE Rules;
 
 # Sensor data
-INSERT INTO Sensor VALUES(NULL, "Temperature1", 1, "", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature2", 1, "", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "WaterLevel1", 1, "", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature3", 1, "", false, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature4", 1, "F", false, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "WaterLevel2", 1, "", false, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature5", 1, "", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature6", 1, "", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature7", 1, "F", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "SoilMoisture1", 1, "%", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "WeatherApiTemperature1", 0, "F", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "WeatherApiHumidity1", 0, "F", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "WeatherApiRain1", 0, "F", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "SoilMoisture2", 1, "%", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature8", 1, "F", false, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "SoilMoisture3", 1, "%", true, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Temperature9", 1, "F", false, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "SoilMoisture4", 1, "", false, NULL, NULL);
-INSERT INTO Sensor VALUES(NULL, "Ultrasonic1", 1, "", true, NULL, NULL);
+INSERT INTO Sensor VALUES(NULL, "Temperature1", 1, "", true);
+INSERT INTO Sensor VALUES(NULL, "Temperature2", 1, "", true);
+INSERT INTO Sensor VALUES(NULL, "WaterLevel1", 1, "", true);
+INSERT INTO Sensor VALUES(NULL, "Temperature3", 1, "", false);
+INSERT INTO Sensor VALUES(NULL, "Temperature4", 1, "F", false);
+INSERT INTO Sensor VALUES(NULL, "WaterLevel2", 1, "", false);
+INSERT INTO Sensor VALUES(NULL, "Temperature5", 1, "", true);
+INSERT INTO Sensor VALUES(NULL, "Temperature6", 1, "", true);
+INSERT INTO Sensor VALUES(NULL, "Temperature7", 1, "F", true);
+INSERT INTO Sensor VALUES(NULL, "SoilMoisture1", 1, "%", true);
+INSERT INTO Sensor VALUES(NULL, "WeatherApiTemperature1", 0, "F", true);
+INSERT INTO Sensor VALUES(NULL, "WeatherApiHumidity1", 0, "F", true);
+INSERT INTO Sensor VALUES(NULL, "WeatherApiRain1", 0, "F", true);
+INSERT INTO Sensor VALUES(NULL, "SoilMoisture2", 1, "%", true);
+INSERT INTO Sensor VALUES(NULL, "Temperature8", 1, "F", false);
+INSERT INTO Sensor VALUES(NULL, "SoilMoisture3", 1, "%", true);
+INSERT INTO Sensor VALUES(NULL, "Temperature9", 1, "F", false);
+INSERT INTO Sensor VALUES(NULL, "SoilMoisture4", 1, "", false);
+INSERT INTO Sensor VALUES(NULL, "Ultrasonic1", 1, "", true);
 
 # Actuator data
 INSERT INTO Actuator VALUES(NULL, "2", "Sprinkler1", false);
