a91f8e784145deefbfbd2de4d5c2b9830ba76c27
sriee <sriee.ammapet@spirent.com>
2017-12-04 17:37:40 -0600

    Iterator for container objects The iterator steps throug the rules table one by one. Forms the container object This container object will be compared for conflict

--- RuleConflict/src/com/parser/ContainerIterator.java
+++ RuleConflict/src/com/parser/ContainerIterator.java
@@ -1,4 +1,50 @@
 package com.parser;
 
-public class ContainerIterator {
+import java.util.Iterator;
+import org.hibernate.Session;
+
+import java.util.NoSuchElementException;
+
+public class ContainerIterator implements Iterable<Container>, Iterator<Container>{
+
+    private long position;
+    private long total;
+    private Factory factory;
+
+    public ContainerIterator() {
+        this.position = 1;
+
+        // TODO: total should be count of the current Rule table
+        // rows
+        this.factory = Factory.instance();
+        Session session = this.factory.getCurrentSession();
+        session.beginTransaction();
+
+        this.total = (long) session.createQuery("select count(*) from Rule").uniqueResult();
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public Iterator<Container> iterator(){ return this; }
+
+    @Override
+    public boolean hasNext(){
+        return this.position <= this.total;
+    }
+
+    @Override
+    public Container next(){
+        if(!this.hasNext()) {
+            this.factory.close();
+            throw new NoSuchElementException();
+        }
+        Container temp = new Container();
+        temp.setName("R" + this.position);
+        this.position++;
+        return temp;
+    }
+
+    @Override
+    public void remove(){ throw new UnsupportedOperationException(); }
 }
