06bde7251ef5859619e6597831ec0e14e86a9184
Sriee <psg.sriee@gmail.com>
2017-12-05 17:20:40 -0600

    Add new to empty Rule table

--- RuleConflict/src/com/json/Driver.java
+++ RuleConflict/src/com/json/Driver.java
@@ -6,12 +6,14 @@ import java.util.List;
 import com.logger.FileLogger;
 import com.parser.Container;
 import com.parser.ContainerIterator;
+import com.parser.Factory;
 import com.rule.Builder;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		Builder builder = new Builder();
+		Factory factory = Factory.instance();
+		Builder builder = new Builder(factory);
 		String jsonExpression = null; 
 		FileLogger log = FileLogger.instance();
 		boolean failed = false;
@@ -20,10 +22,18 @@ public class Driver {
 			jsonExpression = "boolean_expression" + (i + 1) + ".json";
 			log.writeLog("Working on " + jsonExpression);
 			List<Container> containerList = builder.build(jsonExpression);
-			ContainerIterator ci = new ContainerIterator();
+//			ContainerIterator ci = new ContainerIterator();
 			
 			// Should check for rule conflict here
 			for(Container lhs : containerList){
+				
+				if(builder.isRuleTableEmpty()){
+					log.writeLog("Rule Table is empty. Adding first entry.");
+					builder.add(lhs);
+					continue; 
+				}
+				
+				/*
 		        Iterator<Container> iter = ci.iterator();
 		        while(iter.hasNext() && !failed){
 		        	failed = lhs.checkConflict(iter.next());
@@ -32,8 +42,10 @@ public class Driver {
 	        	if(!failed){
 	        		log.writeLog("Rule: " + lhs.getExpression() + " added sucessfully.");
 	        		builder.add(lhs);
-	        	}
+	        	}*/
 			}
 		}
+		
+		factory.close();
 	}
 }
