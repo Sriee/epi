1a52fab704d25b441b736ebba5cf3ee0079c1be0
Sriee <psg.sriee@gmail.com>
2017-12-14 15:43:43 -0600

    Values for demo

--- RuleConflict/resources/load.sql
+++ RuleConflict/resources/load.sql
@@ -20,9 +20,14 @@ INSERT INTO Sensor VALUES(NULL, "SoilMoisture3", 1, "%", true);
 INSERT INTO Sensor VALUES(NULL, "Temperature9", 1, "F", false);
 INSERT INTO Sensor VALUES(NULL, "SoilMoisture4", 1, "", false);
 INSERT INTO Sensor VALUES(NULL, "Ultrasonic1", 1, "", true);
+INSERT INTO Sensor VALUES(NULL, "Time", 0, "", true);
+INSERT INTO Sensor VALUES(NULL, "Infrared", 1, "", true);
+INSERT INTO Sensor VALUES(NULL, "AirQuality", 1, "ug", true);
 
 # Actuator data
 INSERT INTO Actuator VALUES(NULL, "2", "Sprinkler1", false);
 INSERT INTO Actuator VALUES(NULL, "3052CB808C23", "Sprinkler2", true);
 INSERT INTO Actuator VALUES(NULL, "2d774009", "Sprinkler3", true);
 INSERT INTO Actuator VALUES(NULL, "7066147889533359447", "Sprinkler4", true);
+INSERT INTO Actuator VALUES(NULL, "l12", "Lamp", true);
+INSERT INTO Actuator VALUES(NULL, "w1", "Window", true);
