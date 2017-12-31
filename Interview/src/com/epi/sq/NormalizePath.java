d8b1b47ac7c6bc47561ec2ea2c3f5a87a85a0464
Sriee <psg.sriee@gmail.com>
2017-12-30 19:31:20 -0600

    Normalize absolute and relative path

---
+++ Interview/src/com/epi/sq/NormalizePath.java
@@ -0,0 +1,74 @@
+package com.epi.sq;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class NormalizePath {
+	
+	/**
+	 * Leet code problem. Solution -> Accepted
+	 * 
+	 * @param path absolute or relative path in unix style format
+	 * @return absolute path
+	 */
+	private String simplifyPath(String path){
+		
+		StringBuilder sb = new StringBuilder();
+		Deque<String> stack = new LinkedList<>();
+		
+		if(path.startsWith("/"))
+			stack.addFirst("/");
+		
+		String prev = "";
+		String[] symbols = path.split("/");
+		
+		for(String token : symbols){
+			if(token.isEmpty())
+				continue;
+			
+			if(token.equals("."))
+				continue;
+			else if(token.equals("..")){
+				if(stack.isEmpty())
+					stack.addFirst("/");
+				else
+					stack.removeFirst();
+			} else
+				stack.addFirst(token);		
+		}
+		
+		while(!stack.isEmpty()){
+			String temp = stack.removeLast();
+			if(!prev.equals("/")){
+				sb.append("/");
+			}
+			
+			sb.append(temp);
+			prev = temp;
+		}
+		
+		if(sb.length() == 0){
+			return "/";
+		} else {
+			if(sb.charAt(1) == '/')
+				sb.deleteCharAt(1);
+			return sb.toString();
+		}
+	}
+	
+	public static void main(String[] args) {
+		NormalizePath np = new NormalizePath();
+		String[] inputs = new String[]{
+				"/.",
+				"/..",
+				"/home/../../..",
+				"/home/../../../..",
+				"/usr/bin/../lib/gcc",
+				"/a/./b/../../c/"
+		};
+		
+		for(String i : inputs){
+			System.out.println(np.simplifyPath(i));	
+		}
+	}
+}
