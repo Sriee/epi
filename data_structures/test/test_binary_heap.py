3a660fbe1a0a79dc1e1ed104316ec7237b6225d7
Sriee <psg.sriee@gmail.com>
2018-01-08 20:16:19 -0600

    unit test case for binary heap

---
+++ data_structures/test/test_binary_heap.py
@@ -0,0 +1,14 @@
+from lib import binary_heap as bh
+import unittest
+
+class TestBinaryHeap():
+
+    def test_insert(self):
+        mxhp = bh.BinaryHeapMax()
+        mxhp.insert(5)
+        mxhp.insert(9)
+        mxhp.insert(12)
+        print mxhp
+
+if __name__ == "__main__":
+    unittest.main()
