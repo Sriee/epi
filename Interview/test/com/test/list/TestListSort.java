b74bfc25bd725a01e52e26190a719dba42f932f0
sriee <sriee.ammapet@spirent.com>
2018-02-22 10:12:12 -0600

    add unit test for merge sort implementation

--- Interview/test/com/test/list/TestListSort.java
+++ Interview/test/com/test/list/TestListSort.java
@@ -17,7 +17,7 @@ public class TestListSort{
 	}
 
 	@Test
-	public void testEdgeCases(){
+	public void testInsertionSortEdgeCases(){
 		
 		ListNode<Integer> toSort = new ListNode<>(1);
 		
@@ -26,7 +26,7 @@ public class TestListSort{
 	}
 
 	@Test
-	public void testCorrectness(){
+	public void testInsertionSortCorrectness(){
 
 		// To sort
 		ListNode<Integer> toSort = new ListNode<>(Integer.MIN_VALUE);
@@ -59,4 +59,53 @@ public class TestListSort{
 			_so = _so.next;
 		}  
 	}
-}
+
+	@Test
+	public void testMergeSortEdgeCases(){
+		
+		ListNode<Integer> toSort = new ListNode<>(1);
+		
+		assertEquals(toSort, is.mergeSort(toSort));
+		assertEquals(null, is.mergeSort(null));
+		
+		toSort.next = new ListNode<>(-2);
+		ListNode<Integer> sorted = is.mergeSort(toSort);
+		assertEquals(-2, (int)sorted.data);
+		assertEquals(1, (int)sorted.next.data);
+	}
+
+	@Test
+	public void testMergeSortCorrectness(){
+
+		// To sort
+		ListNode<Integer> toSort = new ListNode<>(Integer.MIN_VALUE);
+		toSort.next = new ListNode<>(0);
+		toSort.next.next = new ListNode<>(-41);
+		toSort.next.next.next = new ListNode<>(Integer.MAX_VALUE);
+		toSort.next.next.next.next = new ListNode<>(77);
+		toSort.next.next.next.next.next = new ListNode<>(3167);
+		toSort.next.next.next.next.next.next = new ListNode<>(44);
+		toSort.next.next.next.next.next.next.next = new ListNode<>(-1);
+
+		// Sorted
+		ListNode<Integer> expected = new ListNode<>(Integer.MIN_VALUE);
+		expected.next = new ListNode<>(-41);
+		expected.next.next = new ListNode<>(-1);
+		expected.next.next.next = new ListNode<>(0);
+		expected.next.next.next.next = new ListNode<>(44);
+		expected.next.next.next.next.next = new ListNode<>(77);
+		expected.next.next.next.next.next.next = new ListNode<>(3167);
+		expected.next.next.next.next.next.next.next = new ListNode<>(Integer.MAX_VALUE);
+
+		ListNode<Integer> sorted = is.mergeSort(toSort);
+		assertEquals(8, ListUtil.length(sorted));
+
+		ListNode<Integer> _ex = expected, _so = sorted;
+
+		while(_ex != null){
+			assertEquals(_ex.data, _so.data);
+			_ex = _ex.next;
+			_so = _so.next;
+		}  
+	}
+}       
