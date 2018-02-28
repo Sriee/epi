a4eebaf76b1149139a55cf1c0d1713f768c27ae5
sriee <sriee.ammapet@spirent.com>
2018-02-28 12:32:26 -0600

    binary tree zig zag tree traversal

--- Interview/src/com/epi/tree/Traversal.java
+++ Interview/src/com/epi/tree/Traversal.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Traversal {
+	
 	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> result = new LinkedList<>();
@@ -43,6 +44,14 @@ public class Traversal {
         return result;
 	}
 	
+	/**
+	 * Binary Tree Level Order Traversal
+	 * 
+	 * Leet code. Solution -> Accepted
+	 * 
+	 * @param root Root of the tree
+	 * @return List of nodes for each level 
+	 */
     public List<List<Integer>> levelOrder(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         List<List<Integer>> result = new LinkedList<>();
@@ -71,5 +80,40 @@ public class Traversal {
             }
         }
         return result;
+    }
+
+    public List<List<Integer>> zigZag(TreeNode root){
+    		Queue<TreeNode> queue = new LinkedList<>();
+    		List<List<Integer>> result = new LinkedList<>();
+    		List<Integer> innerList = new LinkedList<>();
+    		int current = 1, next = 0, dir = 0;
+    		queue.add(root);
+    		
+    		while(!queue.isEmpty()) {
+    			TreeNode node = queue.remove();
+    			current--;
+    			
+    			if(node != null) {
+    				if(dir % 2 == 0) {
+    					innerList.add(node.val);
+    				} else {
+    					innerList.add(0, node.val);
+    				}
+    				queue.add(node.left);
+    				queue.add(node.right);
+    				next += 2;
+    			}
+    			
+    			if(current == 0) {
+    				if(!innerList.isEmpty()) {
+    					result.add(innerList);
+    					innerList = new LinkedList<>();
+    				}
+    				current = next;
+    				next = 0;
+    				dir++;
+    			}
+    		}
+    		return result;
     }
 }
