bb25a2ae377c3176817056cbaaeebac0aec2ca16
sriee <sriee.ammapet@spirent.com>
2018-02-28 13:42:08 -0600

    Scratch file

--- Interview/src/com/epi/tree/Scratch.java
+++ Interview/src/com/epi/tree/Scratch.java
@@ -1,55 +1,22 @@
 package com.epi.tree;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.LinkedList;
 
 
 public class Scratch {
 
-    private TreeNode construct(int[] values, int idx){
-        if(idx >= values.length)
-            return null;
-
-        TreeNode node = new TreeNode(values[idx]);
-        node.left = this.construct(values, 2 * idx + 1);
-        node.right = this.construct(values, 2 *idx + 2);
-        return node;
-    }
-
-    public void inOrder(TreeNode root){
-        if(root == null) return;
-
-        this.inOrder(root.left);
-        System.out.print(root.val + " ");
-        this.inOrder(root.right);
-    }
-
-    public void preOrder(TreeNode root){
-        if(root == null) return;
-
-        System.out.print(root.val  + " ");
-        this.preOrder(root.left);
-        this.preOrder(root.right);
-    }
-
-    public void postOrder(TreeNode root){
-        if(root == null) return;
-        this.postOrder(root.left);
-        this.postOrder(root.right);
-        System.out.print(root.val  + " ");
-    }
-
     public static void main(String[] args) {
-        Scratch s = new Scratch();
-        int[] values = new int[3];
-
-        for(int i = 1; i < 4; i++){
-            values[i - 1]= i;
-        }
-
-        System.out.println(Arrays.toString(values));
-        TreeNode root = s.construct(values, 0);
-        System.out.print("InOrder: ");
-        s.preOrder(root);
-
+    		List<Integer> list1 = new LinkedList<>(), list2 = new LinkedList<>();
+    		
+    		list1.add(1);
+    		list1.add(2);
+    		
+    		list2.add(3);
+    		list2.add(4);
+    		
+    		list1.addAll(list2);
+    		
+    		System.out.println(list1.toString());
     }
 }
