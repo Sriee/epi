581b2d6b1f811913b06c3681085de7546a2ba4ff
Sriee Gowthem Raaj A S <psg.sriee@gmail.com>
2018-03-23 12:31:04 -0700

    init for sync application

---
+++ TornExercise/scratch_pad/beacon.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import pytz
+import math
+import os
+
+
+def diff_time(start, end):
+    diff = end - start
+    return math.ceil(diff.seconds / 60)
+
+
+def main():
+    stop, start_time = False, datetime.now(pytz.timezone('US/Pacific')).replace(tzinfo=None)
+    previous = start_time
+    while not stop:
+        current = datetime.now(pytz.timezone('US/Pacific')).replace(tzinfo=None)
+        if diff_time(previous, current) >= 5:
+            with open('temp.txt', 'a') as wp:
+                wp.write(start_time.strftime('%a, %m/%d/%Y %H:%M:%S') + os.linesep)
+            previous = current
+            print('Written to file')
+
+        if diff_time(start_time, current) >= 20:
+            stop = True
+            print('Stopping!..')
+
+
+if __name__ == '__main__':
+    main()
