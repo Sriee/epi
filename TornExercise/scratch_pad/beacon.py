1f2bb5781cd8c11c6e9381db2704d883f9ad8925
Sriee Gowthem Raaj A S <psg.sriee@gmail.com>
2018-03-23 13:03:33 -0700

    time bug correction
    Perforce logic is not required if p4 command line client is installed on the machine

--- TornExercise/scratch_pad/beacon.py
+++ TornExercise/scratch_pad/beacon.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 import pytz
 import math
-import os
 
 # TODO: Enable logging. Add the logging configuration with the program
 # TODO: Add comments (low priority)
-# TODO: Add perforce logic
 
 def diff_time(start, end):
     diff = end - start
@@ -16,14 +14,14 @@ def main():
     stop, start_time = False, datetime.now(pytz.timezone('US/Pacific')).replace(tzinfo=None)
     previous = start_time
     while not stop:
-        current = datetime.now(pytz.timezone('US/Pacific')).replace(tzinfo=None)
-        if diff_time(previous, current) >= 5:
+        current_time = datetime.now(pytz.timezone('US/Pacific')).replace(tzinfo=None)
+        if diff_time(previous, current_time) >= 5:
             with open('temp.txt', 'a') as wp:
-                wp.write(start_time.strftime('%a, %m/%d/%Y %H:%M:%S') + os.linesep)
-            previous = current
+                wp.write(current_time.strftime('%a, %m/%d/%Y %H:%M:%S') + '\n')
+            previous = current_time
             print('Written to file')
 
-        if diff_time(start_time, current) >= 20:
+        if diff_time(start_time, current_time) >= 20:
             stop = True
             print('Stopping!..')
 
