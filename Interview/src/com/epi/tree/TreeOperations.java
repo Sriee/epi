abb9a94ed68d7884ecf480f99c3d0471491a49a0
Sriee Sathiiss <Sriee_Sathiiss@symantec.com>
2019-04-17 11:23:20 -0700

    comments for solved problems

--- Interview/src/com/epi/tree/TreeOperations.java
+++ Interview/src/com/epi/tree/TreeOperations.java
@@ -4,6 +4,24 @@ import java.lang.Math;
 
 public class TreeOperations {
     
+    /**
+	 * Given a Binary Tree check whether its subtree's are symmetric
+	 * 
+	 * Leet code. Solution -> Accepted
+	 * 
+	 * @param root Root of the tree
+	 * @return true if the sub tree's are symmetric, false otherwise
+	 */
+    public boolean isSymmetric(TreeNode root) {
+		if(root == null)
+			return true;
+		
+		return this.isSymmetric(root.left, root.right);
+	}
+
+	/**
+	 * Helper method for checking tree symmetricity
+	 */
 	public boolean isSymmetric(TreeNode p, TreeNode q) {
         if(p == null && q == null)
             return true;
@@ -14,13 +32,14 @@ public class TreeOperations {
         return this.isSymmetric(p.left, q.right) && this.isSymmetric(p.right, q.left);
 	}	
 	
-	public boolean isSymmetric(TreeNode root) {
-		if(root == null)
-			return true;
-		
-		return this.isSymmetric(root.left, root.right);
-	}
-	
+	/**
+	 * Given a Binary Tree check whether the subtree's are same
+	 *
+	 * Leet code. Solution -> Accepted
+	 *
+	 * @param root Root of the tree
+	 * @return true if the sub tree's are same, false otherwise
+	 */ 
 	public boolean isSame(TreeNode root) {
 		if(root == null)
 			return true;
@@ -28,6 +47,9 @@ public class TreeOperations {
 		return this.isSame(root.left, root.right);
 	}
 	
+	/**
+	 * Helper method for checking if sub tree's are same
+	 */
 	public boolean isSame(TreeNode p, TreeNode q) {
 		if(p == null && q == null)
             return true;
@@ -38,6 +60,9 @@ public class TreeOperations {
         return this.isSymmetric(p.left, q.left) && this.isSymmetric(p.right, q.right);
 	}
 	
+	/**
+	 * Helper method to see if the tree is balanced
+	 */ 
 	public BalanceStatus checkBalanced(TreeNode node) {
 		if(node == null)
 			return new BalanceStatus(true, -1);
@@ -54,8 +79,25 @@ public class TreeOperations {
 		
 		return new BalanceStatus(Math.abs(leftChild.height - rightChild.height) <= 1, 1 + Math.max(leftChild.height, rightChild.height));
 	}
-	
+
+	/**
+	 * Given a Binary Tree check is it is Balanced. 
+	 * 	 
+	 * A Tree is balanced if the diff(leftSubtree.height - rightSubtree.height) <= 1.
+	 * 
+	 * Leet Code. Solution -> Accepted
+	 * 
+	 * Note: 
+	 *	The fastest run time solution in leet code uses a global boolean variable to store the result. Also in that approach
+	 *  you will be visiting O(n) nodes for both best & worst case scenarios, whereas in the following approach you will be
+	 *  visiting O(h) for best case and O(n) for worst case scenario. This is because, while traversing if any of the subtree is not
+	 *  balanced, you won't be traversing through the other subtree.
+	 *
+	 * @param node root of the tree
+	 * @return true if the tree is balanced, false otherwise
+	 */	
 	public boolean isBalanced(TreeNode root) {
 		return this.checkBalanced(root).balanced;
 	}
+
 }
