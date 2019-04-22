f0341b79eb8e5612db548f10ab08050914679f5d
sriee <psg.sriee@gmail.com>
2019-04-21 18:04:28 -0700

    trim excess test cases

--- Interview/src/com/epi/sq/CircularDeque.java
+++ Interview/src/com/epi/sq/CircularDeque.java
@@ -2,6 +2,13 @@ package com.epi.sq;
 
 import java.util.Arrays;
 
+/**
+ * Circular Dequeue implementation
+ * 
+ * Leet code. Solution -> Accepted
+ * @author sriee
+ *
+ */
 public class CircularDeque {
 
 	private int head = -1, tail = -1, num = 0;
@@ -99,24 +106,6 @@ public class CircularDeque {
 	
 	// ["insertFront","getRear","deleteLast","getRear","insertFront","insertFront","insertFront","insertFront","isFull","insertFront","isFull"]
 	// [[77],[89],[],[],[],[19],[23],[23],[82],[],[45],[]]
-
-	// ["getRear","deleteLast","getFront","getFront","insertLast","deleteFront","getFront","insertLast","getRear","insertLast","getRear","getFront","getFront","getFront"] 
-	// [[],[],[],[],[74],[],[],[98],[],[99],[],[],[],[]] 
-	
-	// ["getRear","getRear","insertFront","getFront","getFront","getFront","getFront","deleteFront","insertFront","getFront","deleteLast","insertLast","insertLast"]
-	// [[],[],[8],[],[],[],[],[],[75],[],[],[35],[59]] 
-	
-	// ["getRear","getRear","getRear","isEmpty","insertFront","deleteLast","getFront","deleteLast","getRear","getFront","isFull","isFull","deleteFront"] 
-	// [[],[],[],[],[22],[],[],[],[],[],[],[],[]]
-	
-	// ["getFront","deleteLast","getRear","insertFront","getFront","insertFront","insertFront","getRear","isFull","getFront","getFront","insertFront","insertLast"]
-	// [[],[],[],[21],[],[26],[63],[],[],[],[],[87],[76]]
-	
-	// ["getRear","getRear","deleteLast","insertFront","getRear","insertLast","getFront","getFront","getFront","getRear","insertFront","isEmpty","getFront"]
-	// [[],[],[],[26],[],[67],[],[],[],[],[36],[],[]]
-	
-	// ["getFront","insertFront","deleteFront","insertFront","deleteLast","getFront","getRear","getFront","insertFront","getFront","deleteFront","insertFront","isEmpty","getRear","getRear","getRear","getRear","deleteFront","getRear","isEmpty","deleteFront","insertFront","insertLast","deleteLast"]
-	// [[],[72],[],[87],[],[],[],[],[85],[],[],[91],[],[],[],[],[],[],[],[],[],[34],[44],[]]
 	public static void main(String[] args) {
 		// 1
 		CircularDeque c = new CircularDeque(77);
@@ -130,115 +119,6 @@ public class CircularDeque {
 		c.insertFront(82);
 		System.out.println(c.isFull());
 		c.insertFront(45);
-		System.out.println(c.isFull());
-		
-		// 2
-		System.out.println(c.getRear());
-		c.deleteLast();
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		c.insertLast(74);
-		c.deleteFront();
-		System.out.println(c.getFront());
-		c.insertLast(98);
-		System.out.println(c.getRear());
-		c.insertLast(99);
-		System.out.println(c.getRear());
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		
-		// 3
-		System.out.println(c.getRear());
-		System.out.println(c.getRear());
-		c.insertFront(8);
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		c.deleteFront();
-		c.insertFront(75);
-		System.out.println(c.getFront());
-		c.deleteLast();
-		c.insertLast(35);
-		c.insertLast(59);
-		
-		// 4
-		System.out.println(c.getRear());
-		System.out.println(c.getRear());
-		System.out.println(c.getRear());
-		System.out.println(c.isEmpty());
-		c.insertFront(22);
-		c.deleteLast();
-		System.out.println(c.getFront());
-		c.deleteLast();
-		System.out.println(c.getRear());
-		System.out.println(c.getFront());
-		System.out.println(c.isFull());
-		System.out.println(c.isFull());
-		c.deleteFront();
-		
-		// 5
-		System.out.println(c.getFront());
-		c.deleteLast();
-		System.out.println(c.getRear());
-		c.insertFront(22);
-		System.out.println(c.getFront());
-		c.insertFront(26);
-		c.insertFront(63);
-		System.out.println(c.getRear());
-		System.out.println(c.isFull());
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		c.insertFront(87);
-		c.insertLast(76);
-		
-		// 6
-		System.out.println(c.getRear());
-		System.out.println(c.getRear());
-		c.deleteLast();
-		c.insertFront(26);
-		System.out.println(c.getRear());
-		c.insertLast(67);
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		System.out.println(c.getFront());
-		System.out.println(c.getRear());
-		c.insertFront(36);
-		System.out.println(c.isEmpty());
-		System.out.println(c.getFront());
-		
-		// 7
-		// ["getFront","insertFront","deleteFront","insertFront","deleteLast","getFront","getRear","getFront","insertFront","getFront","deleteFront","insertFront"]
-		// [[],[72],[],[87],[],[],[],[],[85],[],[],[91]]
-		System.out.println(c.getFront());
-		c.insertFront(72);
-		c.deleteFront();
-		c.insertFront(87);
-		c.deleteLast();
-		System.out.println(c.getFront());
-		System.out.println(c.getRear());
-		System.out.println(c.getFront());
-		c.insertFront(85);
-		System.out.println(c.getFront());
-		c.deleteFront();
-		c.insertFront(91);
-		
-		// 8
-		// ["isEmpty","getRear","getRear","getRear","getRear","deleteFront","getRear","isEmpty","deleteFront","insertFront","insertLast","deleteLast"]
-		// [[],[],[],[],[],[],[],[],[],[34],[44],[]]
-		System.out.println(c.isEmpty());
-		System.out.println(c.getRear());
-		System.out.println(c.getRear());
-		System.out.println(c.getRear());
-		System.out.println(c.getRear());
-		c.deleteFront();
-		System.out.println(c.getRear());
-		System.out.println(c.isEmpty());
-		c.deleteFront();
-		c.insertFront(34);
-		c.insertLast(44);
-		c.deleteLast();
-		System.out.println(c);
+		System.out.println(c.isFull());		
 	}
-
 }
