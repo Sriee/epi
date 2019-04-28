cb62e210ba986618340ba88c24f5a72745e3a91f
sriee <psg.sriee@gmail.com>
2019-04-28 14:44:58 -0700

    add one row to the tree

--- Interview/src/com/epi/tree/Miscellaneous.java
+++ Interview/src/com/epi/tree/Miscellaneous.java
@@ -38,4 +38,65 @@ public class Miscellaneous {
         
         return this.rootToLeafSum;
     }
+    
+    /**
+     * Helper for adding a row to a tree
+     * 
+     * @param node Current traversed node 
+     * @param v Value for the new node
+     * @param currentDepth depth of the traversed node
+     * @param toAddDepth depth at which new row should be inserted
+     */
+    public void addRowHelper(TreeNode node, int v, int currentDepth, int toAddDepth) {
+        if(node == null)
+            return;
+        
+        if(currentDepth >= toAddDepth)
+            return;
+        
+        TreeNode currentLeft = node.left;
+        TreeNode currentRight = node.right;
+        if(currentDepth == toAddDepth - 1) {    
+            node.left = new TreeNode(v);
+            node.right = new TreeNode(v);
+            node.left.left = currentLeft;
+            node.right.right = currentRight;
+        } else {
+            this.addRowHelper(currentLeft, v, currentDepth + 1, toAddDepth);
+            this.addRowHelper(currentRight, v, currentDepth + 1, toAddDepth);   
+        }
+    }
+    
+    /**
+     * Add one row to the tree
+     * 
+     * At 2 nodes at depth d. 
+     * Left subtree of node at depth d = left subtree of node at depth d - 1. It's right will be null
+     * Right subtree of node at depth d = right subtree of node at depth d - 1. It's left will be null 
+     * 
+     * If depth to insert is at 1. The new root node will be the one to be inserted. 
+     * New root left subtree would be the old root. 
+     * New root right subtree would be null
+     *  
+     * Leet code. Solution -> Accepted
+     * 
+     * @param root of the tree
+     * @param v value for the new node
+     * @param d	depth at which the node to be inserted
+     * @return new root of the tree
+     */
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if(root == null)
+            return null;
+        
+        if(d == 1) {
+            TreeNode temp = root;
+            root = new TreeNode(v);
+            root.left = temp;
+        } else {
+            this.addRowHelper(root, v, 1, d);
+        } 
+            
+        return root;    
+    }
 }
