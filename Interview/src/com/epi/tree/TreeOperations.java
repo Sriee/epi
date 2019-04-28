a8a4f75e8b0854789df2ed46a98ec8c19ca93e5b
sriee <psg.sriee@gmail.com>
2019-04-28 15:20:30 -0700

    subtree check problem

--- Interview/src/com/epi/tree/TreeOperations.java
+++ Interview/src/com/epi/tree/TreeOperations.java
@@ -305,4 +305,27 @@ public class TreeOperations {
     	return 1 + this.countNodes(root.left) + this.countNodes(root.right);
     }
     
+    /**
+     * Given two non-empty binary trees s and t, check whether tree t has exactly
+     * the same structure and node values with a subtree of s. A subtree of s is a 
+     * tree consists of a node in s and all of this node's descendants. The tree s could 
+     * also be considered as a subtree of itself.
+     *  
+     * @param s root of the tree 
+     * @param t root of the subtree 
+     * @return true if t is a subtree of s, false otherwise
+     */
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null && t == null)
+            return true;
+        
+        if(s == null || t == null)
+            return false;
+        
+        boolean res = false;
+        if(s.val == t.val)
+            res = this.isSame(s, t);
+        
+        return res || this.isSubtree(s.left, t) || this.isSubtree(s.right, t);
+    }
 }
