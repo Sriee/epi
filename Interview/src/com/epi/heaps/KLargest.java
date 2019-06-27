5f4493d1dd2a249d94155a54edf4ea16f2734d16
Sriee Sathiiss <Sriee_Sathiiss@symantec.com>
2019-06-26 18:28:33 -0700

    last stone weight

--- Interview/src/com/epi/heaps/KLargest.java
+++ Interview/src/com/epi/heaps/KLargest.java
@@ -1,5 +1,6 @@
 package com.epi.heaps;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class KLargest {
@@ -33,5 +34,41 @@ public class KLargest {
 		}
 
 		return this.queue.peek();
+	}
+	
+	/**
+	 * Leet code problem. Solution -> Accepted
+	 * 
+	 * Each turn, choose the two heaviest rocks
+	 * 
+	 * If x == y, it cancels out each other;
+	 * If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x. At the end, there is at most 1 stone left.  
+	 * 
+	 * Return the weight of the stone (or 0 if there are no stones left.)
+	 * 
+	 * @param stones array of weights
+	 * @return the weight of the stone
+	 */
+	public int lastStoneWeight(int[] stones) {
+		PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer i, Integer j) {
+				return j.compareTo(i);
+			}
+		});
+		
+		for(int i : stones)
+			queue.add(i);
+		
+		while(queue.size() > 1) {
+			int x = queue.poll();
+			int y = queue.poll();
+			
+			if(x != y) {
+				queue.add(Math.abs(x - y));
+			}
+		}
+		
+		return queue.isEmpty() ? 0 : queue.peek();
 	}
 }
