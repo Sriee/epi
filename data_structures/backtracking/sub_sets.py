f3d42de70e4bd739befc9e6b385ab59cf54f014c
sriee <psg.sriee@gmail.com>
2019-07-17 20:51:56 -0700

    power set of a set with duplicates

--- data_structures/backtracking/sub_sets.py
+++ data_structures/backtracking/sub_sets.py
@@ -26,3 +26,63 @@ def subsets(nums):
     subset_helper(nums, 0, [], res)
 
     return res
+
+
+def subsets_duplicates(nums):
+    """
+    Leet code. Solution -> Accepted
+
+    Given an array find all possible subsets of an array. The given array contains
+    duplicate entries. Order of the output doesn't matter. Output should not contain
+    any duplicates.
+
+    Run time: 44 ms. Little puzzled about the run time.
+        - We are sorting the input array
+        - Converting list to tuple + Hashing a tuple when adding it to result
+        - Converting tuple entries back to set while returning the result yet the run
+        time seems to be the same as above sub set sum problem
+
+    Example:
+        [4, 4, 4, 1, 4] -> [[], [1], [1, 4], [1, 4, 4], [1, 4, 4, 4], [1, 4, 4, 4, 4],
+                            [4], [4, 4], [4, 4, 4], [4, 4, 4, 4]]
+
+    :param nums: Arrays with duplicates
+    :return: subsets of the array without duplicates
+    """
+    res = set()
+
+    def subset_helper(nums, idx, path, res):
+        res.add(tuple(path.copy()))
+
+        for i in range(idx, len(nums)):
+            path.append(nums[i])
+            subset_helper(nums, i + 1, path, res)
+            path.pop()
+    subset_helper(sorted(nums), 0, [], res)
+
+    return [list(i) for i in res]
+
+
+def subsets_duplicates_without_set(nums):
+    """
+    Leet code. Solution -> Accepted
+
+    Run time: 40 ms. Checking whether the path is visited seems to improve time and
+    space complexity. Space is significantly improved since we won't be traversing
+    redundant branches
+
+    :param nums: Array with duplicates
+    :return: subsets of the array without duplicates
+    """
+    res = []
+
+    def subset_helper(nums, idx, path, res):
+        res.append(path.copy())
+
+        for i in range(idx, len(nums)):
+            path.append(nums[i])
+            if path not in res:
+                subset_helper(nums, i + 1, path, res)
+            path.pop()
+
+    subset_helper(sorted(nums), 0, [], res)
