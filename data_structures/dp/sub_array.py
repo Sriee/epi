326d5244808e1469b3891782103cff34d75e269a
sriee <psg.sriee@gmail.com>
2019-08-17 21:23:41 -0700

    longest increasing sub-sequence (worst run time)

--- data_structures/dp/sub_array.py
+++ data_structures/dp/sub_array.py
@@ -1,3 +1,34 @@
+def longest_increasing_sub_sequence(nums):
+    """
+    Run Time: 1252 ms. Worst Run Time. Time complexity is O(n^2). The below 
+    solution is based on Skienna's Algorithm Design Manual. But the run time
+    is not optimal. 
+
+    Given an array. Find the longest increasing subsequence. 
+    Note the difference between sub-sequence and a sub-array
+
+    Example:
+        [-2, 1, 3, 8, 2, 0, 9] -> [1, 3, 8, 9]
+
+    :param nums: Given array
+    :return: Longest increasing sub sequence
+    """
+    if len(nums) == 0:
+        return
+    mem, j, res = [1] * len(nums), 1, 0
+    while j < len(nums):
+        i = 0
+        while i < j:
+            if nums[i] < nums[j]:
+                mem[j] = max(mem[j], mem[i] + 1)
+            i += 1
+        if mem[j] > res:
+            res = mem[j]
+        j += 1
+
+    return res
+
+
 if __name__ == '__main__':
     nums = [-2, 1, -3, 4]
     cur_max = nums[0]
