f44c80cd55ecc268736e9adb7789be427a98f3aa
sriee <psg.sriee@gmail.com>
2019-08-26 23:00:12 -0700

    reverse bits in a binary number

--- Interview/src/com/epi/primitives/ReverseBits.java
+++ Interview/src/com/epi/primitives/ReverseBits.java
@@ -5,6 +5,33 @@ import java.util.Scanner;
 
 public class ReverseBits {
 
+	/**
+	 * Leet code. Solution -> Accepted
+	 * 
+	 * Run Time: 0 ms. Optimal solution. 
+	 * 
+	 * Given a number do a bit wise complement on that number
+	 * 
+	 * Example:
+	 * 		Input: 5 (b'101) -> 2 (b'010)
+	 *  
+	 * @param N Number
+	 */
+	public int bitwiseComplement(int N) {
+		int res = 0, pos = 0;
+		
+		while(N != 0) {
+			if((N & 1) == 0) {
+				res += (1 << pos);
+			}
+			
+			N >>= 1;
+			pos++;
+		}
+		
+		return res;
+	}
+	
 	public long reverseBits(long number){
 		if(number < 0) throw new IllegalArgumentException("Number should be positive.");
 		return((number >> 48) & 0xFFFF) |
@@ -15,11 +42,11 @@ public class ReverseBits {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		ReverseBits rev = null;
+		ReverseBits rev = new ReverseBits();
+		
 		try{
 			System.out.print("Enter the number: ");
 			long number = scan.nextLong();
-			rev = new ReverseBits();
 			System.out.println(rev.reverseBits(number));
 		} catch (InputMismatchException e){
 			System.out.println("Invalid input.");
@@ -30,7 +57,7 @@ public class ReverseBits {
 		} finally {
 			scan.close();
 		}
-
+		System.out.println(rev.bitwiseComplement(10000));
 	}
 
 }
