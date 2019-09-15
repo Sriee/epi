5af89b16443a0c8f1707f4748f2aefe18404cfc3
sriee <psg.sriee@gmail.com>
2019-09-15 13:08:07 -0700

    search in bst

--- data_structures/lib/binary_search.py
+++ data_structures/lib/binary_search.py
@@ -78,6 +78,61 @@ class BinarySearchTree(object):
             self.root = TreeNode(val)
         self._size += 1
 
+    def search_iterative(self, node, val):
+        """Search in BST (iterative implementation)
+
+        :param node: current node
+        :param val: val to find
+        :return: node if found else None
+        """
+        current = node
+        while current:
+            if val < current.val:
+                current = current.left
+            elif val > current.val:
+                current = current.right
+            else:
+                return current
+
+        return None
+
+    def search_recursive(self, node, val):
+        """Search in BST (recursive implementation)
+
+        :param node: current node
+        :param val: val to find
+        :return: node if found else None
+        """
+        if not node:
+            return None
+
+        # Trick: By checking < & > we are avoiding multiple checks for equality
+        # followed by < & > check. This way helps to increase the run time
+        if val < node.val:
+            return self.search_recursive(node.left, val)
+        elif val > node.val:
+            return self.search_recursive(node.right, val)
+        else:
+            return node
+
+    def search(self, val):
+        """Search in BST"""
+
+        # Randomly select iterative/recursive implementation
+        if randint(0, 1) == 0:
+            return self.search_recursive(self.root, val)
+        else:
+            return self.search_iterative(self.root, val)
+
+    def __contains__(self, item):
+        """Search in BST
+
+        :param item: val to search
+        :return: True if the node is found False otherwise
+        """
+        node = self.search(item)
+        return True if node else False
+
     def __len__(self):
         return self._size
 
