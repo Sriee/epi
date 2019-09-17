84dec3255ea60e4bb1bab8f8e0d0e4f6181ccfeb
sriee <psg.sriee@gmail.com>
2019-09-16 18:00:20 -0700

    convert sorted linked list to bst

--- Interview/src/tree/BinarySearchTree.java
+++ Interview/src/tree/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import list.ListNode;
+
 public class BinarySearchTree {
 
 	/**
@@ -35,6 +40,33 @@ public class BinarySearchTree {
 	 */
 	public TreeNode sortedArrayToBST(int[] nums) {
 		return this.helper(nums, 0, nums.length - 1);
+	}
+
+	/**
+	 * Leet code. Solution -> Accepted
+	 * 
+	 * Run Time: 2 ms. Below Average Run Time
+	 * 
+	 * Given a linked list which is sorted create a height balanced binary search
+	 * tree.
+	 * 
+	 * @param head sorted linked list
+	 * @return root of constructed binary search tree
+	 */
+	public TreeNode sortedListtoBST(ListNode<Integer> head) {
+		List<Integer> list = new ArrayList<>();
+		ListNode<Integer> iter = head;
+
+		while (iter != null) {
+			list.add(iter.data);
+			iter = iter.next;
+		}
+
+		// Note: Converting list to int increases the run time from 2 ms to 37 ms
+		// Retaining the conversion to use the existing helper method
+		int[] nums = list.stream().mapToInt(i -> i).toArray();
+
+		return this.helper(nums, 0, list.size() - 1);
 	}
 
 	public static void main(String[] args) {
