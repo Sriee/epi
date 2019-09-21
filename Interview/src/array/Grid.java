94fbda01ef50da304ce4487436e04e276f3679b3
sriee <psg.sriee@gmail.com>
2019-09-21 10:18:02 -0700

    shortest path binary matrix (above avg run time)

--- Interview/src/array/Grid.java
+++ Interview/src/array/Grid.java
@@ -56,63 +56,57 @@ public class Grid {
 		return matrix;
 	} 
 	
+	/**
+	 * Leet code. Solution -> Accepted
+	 * 
+	 * Run Time: 20 ms. Above average run time.
+	 * 
+	 * Given a binary matrix find the shortest path from top-left to bottom right. Path is taken 
+	 * via the cells which have 0 in them. Diagonal traversal of the cell is permitted. 
+	 * 
+	 * @param grid m x n matrix
+	 * @return shortest distance from top-left to bottom right else - 1
+	 */
 	private int shortestPathBinaryMatrix(int[][] grid) {
 		int m = grid.length, n = grid[0].length, i = 0, j = 0;
+		
+		if(grid[0][0] == 1 || grid[m - 1][n - 1] == 1)
+			return -1;
+		
 		Queue<int[]> queue = new LinkedList<>();
 		int[][] directions = new int[][] {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}};
 		
 		for(i = 0; i < m; i++) {
 			for(j = 0; j < n; j++) {
-				/*
-				if(grid[i][j] == 1)
+				if(grid[i][j] == 1) {
 					grid[i][j] = -1;
-				*/
-				if(i == 0 && j == 0) {
-					grid[i][j] = 1;
-					queue.add(new int[] {i, j});
-					continue;
 				}
-					
-				
-				if(grid[i][j] == 0) {
-					queue.add(new int[] {i, j});
-					grid[i][j] = Integer.MAX_VALUE;	
-				} else
-					grid[i][j] = Integer.MIN_VALUE; 
 			}
 		}
+
+		grid[0][0] = 1;
+		queue.add(new int[] {0, 0});
 		
 		while(!queue.isEmpty()) {
+			
 			int[] current = queue.remove();
 			
 			for(int[] d : directions) {
 				i = d[0] + current[0];
 				j = d[1] + current[1];
 				
-				if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == Integer.MIN_VALUE || grid[i][j] <= grid[current[0]][current[1]] + 1)
+				if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) {
 					continue;
+				}
 				
-				grid[i][j] = grid[current[0]][current[1]] + 1;
-				queue.add(new int[] {i, j});
+				if(grid[i][j] == 0 || grid[i][j] > grid[current[0]][current[1]] + 1) {
+					grid[i][j] = grid[current[0]][current[1]] + 1;
+					queue.add(new int[] {i, j});
+				}
 			}
-			
-			System.out.println("=============================");
-			System.out.println(Arrays.toString(current) + "\n");
-			this.printMatrix(grid);
 		}
 		
-		// System.out.println();
-		// this.printMatrix(grid);
-		return (grid[m - 1][n - 1] == Integer.MIN_VALUE) ? -1 : grid[m - 1][n - 1];
-	}
-	
-	private void printMatrix(int[][] matrix) {
-		for(int i = 0; i < matrix.length; i++) {
-			for(int j = 0; j < matrix[0].length; j++) {
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
-		}	
+		return (grid[m - 1][n - 1] == 0) ? -1 : grid[m - 1][n - 1];
 	}
 	
 	public static void main(String[] args) {
@@ -124,7 +118,7 @@ public class Grid {
 			{1, 0, 1, 1, 0, 1},
 			{1, 0, 0, 1, 1, 0}
 		};
-		/*
+		
 		matrix = g.updateMatrix(matrix);
 		for(int i = 0; i < matrix.length; i++) {
 			for(int j = 0; j < matrix[0].length; j++) {
@@ -132,7 +126,7 @@ public class Grid {
 			}
 			System.out.println();
 		}
-		*/
+		
 		System.out.println(g.shortestPathBinaryMatrix(matrix));
 	}
 }
