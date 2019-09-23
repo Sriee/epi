b5196b2d17f9cd2a95a76d26319fc0cf74a04b99
sriee <psg.sriee@gmail.com>
2019-09-22 22:44:35 -0700

    clone graph (avg run time)

--- Interview/src/graph/GraphProblems.java
+++ Interview/src/graph/GraphProblems.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class GraphProblems {
 
 	/**
-	 * Leet code. Solution -> Wrong Answer
+	 * Leet code. Solution -> Accepted
 	 * 
-	 * Could not find out the reason
+	 * Run Time: 2 ms. Average run time. Best run time uses DFS
 	 * 
 	 * Clone a graph
 	 * 
@@ -18,64 +18,27 @@ public class GraphProblems {
 		if (node == null)
 			return null;
 
-		Set<Node> isCloned = new HashSet<>();
 		Map<Node, Node> map = new HashMap<>();
 		Queue<Node> queue = new LinkedList<>();
-
-		Node current = null, clonedCurrent = null, newNbr = null;
+		
+		Node cloned = new Node(node.val, new LinkedList<>()), current = null;
+		map.put(node, cloned);
 		queue.add(node);
 
 		while (!queue.isEmpty()) {
 			current = queue.remove();
-
-			if (map.containsKey(current)) {
-				clonedCurrent = map.get(current);
-			} else {
-				clonedCurrent = new Node(current.val, new LinkedList<>());
-				map.put(current, clonedCurrent);
-			}
-
 			for (Node nbr : current.neighbors) {
-				if (!isCloned.contains(nbr)) {
-
-					if (map.containsKey(nbr)) {
-						newNbr = map.get(nbr);
-						newNbr.neighbors.add(clonedCurrent);
-						clonedCurrent.neighbors.add(newNbr);
-					} else {
-						newNbr = new Node(nbr.val, new LinkedList<>());
-						newNbr.neighbors.add(clonedCurrent);
-						clonedCurrent.neighbors.add(newNbr);
-						map.put(nbr, newNbr);
-					}
-
+				
+				if (!map.containsKey(nbr)) {
+					map.put(nbr, new Node(nbr.val, new LinkedList<>()));
 					queue.add(nbr);
 				}
+				
+				map.get(current).neighbors.add(map.get(nbr));
+				
 			}
-
-			isCloned.add(current);
 		}
-		/*
-		 * System.out.println("Given"); for(Map.Entry<Node, Node> entry: map.entrySet())
-		 * { current = entry.getKey();
-		 * 
-		 * System.out.print(current.val + " " + current.toString() +
-		 * " is connected to ["); for(Node nbr : current.neighbors)
-		 * System.out.print(nbr.val + " " + nbr.toString() + " ");
-		 * 
-		 * System.out.println("]"); }
-		 * 
-		 * System.out.println("\nCloned"); for(Map.Entry<Node, Node> entry:
-		 * map.entrySet()) { current = entry.getValue();
-		 * 
-		 * System.out.print(current.val + " " + current.toString() +
-		 * " is connected to ["); for(Node nbr : current.neighbors)
-		 * System.out.print(nbr.val + " " + nbr.toString() + " ");
-		 * 
-		 * System.out.println("]"); }
-		 */
-
-		return map.get(node);
+		return cloned;
 	}
 
 	public static void main(String[] args) {
