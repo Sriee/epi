30218166b8bfe72c2e40516ff91760a8518b7347
sriee <psg.sriee@gmail.com>
2019-09-24 18:36:52 -0700

    strongly connected component

--- data_structures/graphs/graph_problems.py
+++ data_structures/graphs/graph_problems.py
@@ -1,7 +1,51 @@
 from lib.graph import DFSGraph
 
+time = 0
 
-if __name__ == '__main__':
+
+def helper(vertex, component):
+    global time
+
+    component.append(vertex.label)
+    vertex.visited = True
+    time += 1
+    vertex.discovery = time
+
+    neighbors = sorted(vertex.neighbors, key=lambda k: k.finish)
+
+    for nbr in neighbors:
+        if not nbr.visited:
+            helper(nbr, component)
+
+    time += 1
+    vertex.finish = time
+
+
+def strongly_connected_components(g):
+    # DFS to find start/finish time
+    g.dfs()
+
+    # Transpose the graph
+    gt = DFSGraph()
+    for u in g:
+        for v in u.neighbors:
+            gt.add_edge(v.label, u.label)
+
+    res = []
+    for u in gt:
+        component = []
+        if not u.visited:
+            helper(u, component)
+
+        if component:
+            res.append(component)
+
+    print('Printing Components')
+    for i, c in enumerate(res):
+        print(i, c)
+
+
+def topological_sort():
     g = DFSGraph()
 
     g.add_edge("A", "B")
@@ -25,3 +69,23 @@ if __name__ == '__main__':
 
     for i, j in enumerate(top_sort):
         print('{}    {}'.format(i, j))
+
+
+if __name__ == '__main__':
+    gp = DFSGraph()
+
+    gp.add_edge("A", "B")
+    gp.add_edge("B", "C")
+    gp.add_edge("B", "E")
+    gp.add_edge("C", "F")
+    gp.add_edge("C", "C")
+    gp.add_edge("D", "B")
+    gp.add_edge("D", "G")
+    gp.add_edge("E", "D")
+    gp.add_edge("E", "A")
+    gp.add_edge("F", "H")
+    gp.add_edge("G", "E")
+    gp.add_edge("H", "I")
+    gp.add_edge("I", "F")
+
+    strongly_connected_components(gp)
