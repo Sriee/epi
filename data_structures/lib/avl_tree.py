dc434ee5d8c4758a65d2e3f87e3e38b8ef25ac20
sriee <psg.sriee@gmail.com>
2019-10-07 23:01:36 -0700

    double rotation

--- data_structures/lib/avl_tree.py
+++ data_structures/lib/avl_tree.py
@@ -7,20 +7,18 @@ class AvlNode(object):
         self.height = 0
         self.occurrence = 0
 
-
     def __str__(self):
         s = 'AvlNode({}, left={}, right={}, height={}, occurrence={})'.format(
             self.val,
-            self.left.val if self.left else None,
-            self.right.val if self.right else None,
+            self.left.val if self.val else None,
+            self.right.val if self.val else None,
             self.height,
             self.occurrence
         )
         return s
 
 
-
-class AVLTree(object):
+class AvlTree(object):
 
     def __init__(self):
         self.root = None
@@ -31,62 +29,133 @@ class AVLTree(object):
         return self._size
 
     def balance(self, node: AvlNode):
-        if not node:
-            return node
 
+        if self.balance_factor(node) > 1:
+            # Handle left heavy tree
 
-        node.height = max( self._height(node.left), self._height(node.right) ) + 1
+            if self.balance_factor(node.left) < 0:
+                """Case 2: Left - Right
+                    
+                    1           1
+                   /           / 
+                  2    -->    3  
+                   \         /
+                    3       2
+                """
+                node = self.rotate_left(node)
+
+            # Case 1: Left - Left
+            node = self.rotate_right(node)
+        elif self.balance_factor(node) < -1:
+            # Handle right heavy tree
+            if self.balance_factor(node.right) > 0:
+                """Case 3: Right - Left
+
+                    1         1
+                     \         \
+                      2  -->    3
+                     /           \
+                    3             2
+                """
+                node = self.rotate_right(node)
+
+            # Case 4: Right - Right
+            node = self.rotate_left(node)
         return node
 
-    def rotate_right(self, k2: AvlNode):
-        k1 = k2.left
-        k2.left = k1.right
-        k1.right = k2
-        k2.height = max( self._height(k2.left), self._height(k2.right) ) + 1
-        k1.height = max( self._height(k1.left), k2.height ) + 1
-        return k1
+    def balance_factor(self, node: AvlNode):
+        return self._height(node.left) - self._height(node.right)
 
-    def rotate_left(self, k2: AvlNode):
-        k1 = k2.right
-        k2.right = k1.left
-        k1.left = k2
-        k2.height = max( self._height(k2.left), self._height(k2.right) ) + 1
-        k1.height = max( self._height(k1.right), k2.height ) + 1
-        return k1
-        
-    def insert(self, x, node: AvlNode):
+    def contains(self, node, val):
         if not node:
-            return AvlNode(x)
+            return False
 
-        if x.val < node.val:
-            node.left = self.insert(x, node.left)
-        elif x.val > node.val:
-            node.right = self.insert(x, node.right)
+        if val < node.val:
+            return self.contains(node.left, val)
+        elif val > node.val:
+            return self.contains(node.right, val)
         else:
-            node.occurrence += 1
+            return True
 
-        return self.balance(node)
+    def height(self):
+        return self._height(self.root)
 
-    def _height(self, node):
+    def _height(self, node: AvlNode):
         return node.height if node else -1
 
     def is_empty(self):
-        return self.size == 0
+        return self._size == 0
 
-    def in_order(self, node):
-        if node:
-            print(node)
-            self.in_order(node.left)
-            self.in_order(node.right)
+    def put(self, x):
+        self.root = self._put(self.root, x)
+        self._size += 1
+
+    def _put(self, node: AvlNode, x):
+        if not node:
+            return AvlNode(x)
+
+        if x < node.val:
+            return self._put(node.left, x)
+        elif x > node.val:
+            return self._put(node.right, x)
+        else:   # Found duplicate node
+            node.occurrence += 1
+
+        # Update height
+        node.height = max(self._height(node.left), self._height(node.right)) + 1
+        self.balance(node)  # Balance the node
+
+    def rotate_left(self, k2: AvlNode):
+        """Rotate left
+
+            1               2
+             \             / \
+              2     -->   1   3
+               \
+                3
+
+        :param k2:
+        :return:
+        """
+        k1: AvlNode = k2.right
+
+        k2.right = k1.left
+        k1.left = k2
+
+        # Update heights
+        k2.height = max(self._height(k2.left), self._height(k2.right)) + 1
+        k1.height = max(k2.height, self._height(k1.right)) + 1
+        return k1
+
+    def rotate_right(self, k2: AvlNode):
+        """Rotate right
+                1             2
+               /             / \
+              2     -->     1   3
+             /
+            3
+
+        :param k2:
+        :return:
+        """
+        k1: AvlNode = k2.left
+
+        # Since we are assigning k2's left to k1 it is guaranteed that k2's left
+        # is empty. Also k1's right is guaranteed to be < k2
+        k2.left = k1.right
+        k1.right = k2
+
+        # Update height's of k1 & k2
+        k2.height = max(self._height(k2.left), self._height(k2.right)) + 1
+        k1.height = max(self._height(k1.left), k2.height) + 1
+        return k1
+
+    def __contains__(self, val):
+        return self.contains(self.root, val)
 
     def __len__(self):
-        return self.size
+        return self._size
 
 
 if __name__ == '__main__':
-    tree = AVLTree()
-    t = AvlNode(3)
-    t.left = AvlNode(2)
-    t.left.left = AvlNode(1)
-    after = tree.rotate_right(t)
-    tree.in_order(after)
+    a = AvlTree()
