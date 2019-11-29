b467fced544757cf03eb3f094fdbdd27cd978a44
sriee <psg.sriee@gmail.com>
2019-11-29 12:15:05 -0800

    optimal solution for hamming weight

--- Interview/src/primitives/NumberProblems.java
+++ Interview/src/primitives/NumberProblems.java
@@ -21,6 +21,20 @@ public class NumberProblems {
                 count++;
             i <<= 1;
         }
+        
+        /**
+         * while(n != 0) {
+         * 		count = count + (n & 1);
+         * 	
+         * 		// This will lead to an infinite loop. While right shifting
+         * 		// Java appends '1' to the left most bit. 
+         * 		n >> = 1;
+         * 
+         *  	// Instead use right shift operator which fills '0' to its
+         *  	// left most bit
+         *  	n >>> = 1; 	
+         * }
+         */
         return count; 
     }
     
