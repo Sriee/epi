b14eda9564edf2d72dcbcfa3d2e349d8df98e18d
sriee <psg.sriee@gmail.com>
2019-11-28 16:15:01 -0800

    count primes

--- Interview/src/primitives/PrimeGen.java
+++ Interview/src/primitives/PrimeGen.java
@@ -59,18 +59,45 @@ public class PrimeGen {
 		return primes.toString().substring(1, len - 1);
 	}
 	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		long n = -1;
-		PrimeGen pg = null;
-		try{
-			n = scan.nextLong();
-			pg = new PrimeGen();
-			System.out.println(pg.generate(n));
-		} catch(Exception e){
-			e.printStackTrace();
-		} finally {
-			scan.close();
+	/**
+	 * Leet code. Solution -> Accepted
+	 * 
+	 * Run Time: 10 ms. Above average run time
+	 * 
+	 * A simpler version of using seive of eratosthene. Could use this for generating primes as well.
+	 * @param n
+	 * @return
+	 */
+	public int countPrimes(int n) {
+		if(n <= 2)
+			return 0;
+		
+		if(n <= 3)
+			return 1;
+		
+		int count = 0;
+		boolean[] notPrime = new boolean[n];
+		notPrime[0] = true;
+		notPrime[1] = true;
+
+		for(int i = 2; i < Math.sqrt(n); i++) {
+			if(!notPrime[i]) {
+				for(int j = 2; j * i < n; j++) {
+					notPrime[i * j] = true;
+				}
+			}
 		}
+		
+		for(int i = 2; i < n; i++) {
+			if(!notPrime[i]) count++;
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		PrimeGen pg = new PrimeGen();
+		int a = pg.countPrimes(15);
+		System.out.println(a);
 	}
 }
+
