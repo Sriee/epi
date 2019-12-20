6fcfe8549ce3cd645a03889fed0bddf2ee5878a7
sriee <psg.sriee@gmail.com>
2019-12-19 18:51:55 -0800

    rod max cut

--- Interview/src/dp/BuySellStock.java
+++ Interview/src/dp/BuySellStock.java
@@ -1,7 +1,24 @@
 package dp;
 
+import java.time.Instant;
+import java.time.Duration;
+import java.util.Random;
+
+
 public class BuySellStock {
 
+	public int maxCut(int[] prices, int n) {
+		if(n == 0)
+			return 0;
+		
+		int q = Integer.MIN_VALUE;
+		for(int i = 1; i <= n; i++) {
+			q = Math.max(q, prices[i] + maxCut(prices, n - i));
+		}
+		
+		return q;
+	}
+	
 	/**
 	 * Leet code. Solution -> Accepted
 	 * 
@@ -25,11 +42,31 @@ public class BuySellStock {
 		return max;
 	}
 
+	private int[] generate(int n) {
+		int[] arr = new int[n];
+		Random rand = new Random();
+		
+		for(int i = 0; i < n; i++) 
+			arr[i] = rand.nextInt(100);
+		
+		return arr;
+	}
 	public static void main(String[] args) {
 		BuySellStock stock = new BuySellStock();
+		int[] prices = null;
+		/*
 		int[] prices = new int[] { 17, 15, 1, 17, 9, 5, 23, 12, 7, 2, 25, 12, 23, 19, 21, 23, 21, 14, 22, 24 };
-
 		System.out.println(stock.maxProfit(prices));
+		*/
+		// prices = new int[] {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+		prices = stock.generate(50);
+		
+		Instant start = Instant.now();
+		int result = stock.maxCut(prices, 35);
+		Instant end = Instant.now();
+		Duration timeLapsed = Duration.between(start, end);
+		
+		System.out.println(result + "\nTime: " + timeLapsed.getSeconds());
 	}
 
 }
