aba98e6ea4d0f5a59784609903a49eac84ece9f1
sriee <psg.sriee@gmail.com>
2021-04-08 05:20:18 -0400

    Segment Tree Implementation

--- Interview/src/tree/SegmentTree.java
+++ Interview/src/tree/SegmentTree.java
@@ -1,124 +1,69 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-
 public class SegmentTree {
 
-    public int build(int[] heights, int[] st, int start, int end, int index) {
+    int[] st; // Data structure to hold the segment tree
+
+    // Initialize the segment tree
+    public SegmentTree(int capacity) { this.st = new int[capacity]; }
+
+    private void build(int[] arr, int start, int end, int index) {
         if(start > end)
-            return -1;
-        
+            return;
+
         if(start == end) {
             st[index] = start;
         } else {
             int mid = start + (end - start) / 2;
-            st[index] = Math.min(build(heights, st, start, mid, 2*index + 1), build(heights, st, start, mid, 2*index + 2));
+            build(arr, start, mid, 2 * index + 1);
+            build(arr, mid + 1, end, 2 * index + 2);
+
+            int left = st[2 * index + 1];
+            int right = st[2 * index + 2];
+
+            /* Change the below build logic based on the type of segment tree you are constructing.
+             * Example:
+             *   Sum queries: st[index] = arr[left] + arr[right];
+             *   Max queries: st[index] = arr[left] > arr[right] ? left : right;
+             *
+             * Here we are constructing a segment tree to give us minimum value of a segment
+             */
+            st[index] = arr[left] < arr[right] ? left : right;
         }
-        
-        return st[index];
     }
 
-    public int findMinIndex(int[] heights, int[] st, int start, int end, int queryStart, int queryEnd, int index) {
-        if(queryStart <= start && queryEnd >= end)
+    public void buildSegmentTree(int[] arr) { build(arr, 0, arr.length - 1, 0); }
+
+    private int findMinIndex(int[] arr, int qStart, int qEnd, int start, int end, int index) {
+        /*
+         * 4 cases of segment positions are considered below
+         *
+         * [qS, qE] | [qS, qE]    |    [qS, qE] |   [qS, qE]
+         * [st, en] |    [st, en] | [st, en]    | [start,  end]
+         */
+        if(qStart <= start && qEnd >= end)
             return st[index];
-        
-        if(end < queryStart || start > queryEnd)
+
+        // [qS, qE] < [s, e] > [qS, qE]
+        if(qStart > end || qEnd < start)
             return -1;
-        
+
         int mid = start + (end - start) / 2;
-        int l = findMinIndex(heights, st, start, mid, queryStart, queryEnd, 2 * index + 1);
-        int r = findMinIndex(heights, st, mid + 1, end, queryStart, queryEnd, 2 * index + 2);
+        int left = findMinIndex(arr, qStart, qEnd, start, mid, 2 * index + 1);
+        int right = findMinIndex(arr, qStart, qEnd, mid + 1, end, 2 * index + 2);
 
-        if(l == -1)
-            return r;
-        else if(r == -1)
-            return l;
+        // Note: we are returning the index since we store the indexes of the array elements in the segment tree
+        // not the element themselves.
+        if(left == -1)
+            return right;
+        else if(right == -1)
+            return left;
         else
-            return (heights[l] < heights[r]) ? l : r;
+            return (arr[left] < arr[right]) ? left : right;
     }
 
-    public SegmentTreeNode buildTree(int[] heights, int start, int end) {
-        if(start > end)
-            return null;
-
-        SegmentTreeNode node = new SegmentTreeNode(start, end);
-
-        if(start == end) {
-            node.minIndex = start;
-        } else {
-            int mid = start + (end - start) / 2;
-
-            node.left = buildTree(heights, start, mid);
-            node.right = buildTree(heights, mid + 1, end);
-
-            if (node.left != null && node.right != null)
-                node.minIndex = (heights[node.left.minIndex] < heights[node.right.minIndex]) ? node.left.minIndex : node.right.minIndex;
-            else if (node.left == null)
-                node.minIndex = node.right.minIndex;
-            else
-                node.minIndex = node.left.minIndex;
-        }
-
-        return node;
-    }
-    
-    public int findIndex(int[] heights, SegmentTreeNode node, int start, int end) {
-        if(node == null || start > end || end < node.start || start > node.end)
-            return -1;
-
-        if(start == node.start && end == node.end)
-            return node.minIndex;
-
-        int mid = node.start + (node.end - node.start) / 2;
-        int l = findIndex(heights, node.left, start, Math.min(mid, end));
-        int r = findIndex(heights, node.right, Math.max(start, mid + 1), end);
-
-        if(l == -1)
-            return r;
-        else if(r == -1)
-            return l;
-        else
-            return (heights[l] < heights[r]) ? l : r;
-    }
-    
-    // Print the segment tree
-    public List<List<int[]>> levelOrder(SegmentTreeNode root) {
-        Queue<SegmentTreeNode> queue = new LinkedList<>();
-        List<List<int[]>> result = new ArrayList<>();
-        List<int[]> innerList;
-
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            innerList = new ArrayList<>();
-            
-            for(int i = 0; i < size; i++) {
-                SegmentTreeNode node = queue.remove();
-                
-                int[] trio = new int[]{ node.start, node.end, node.minIndex};
-                innerList.add(trio);
-                
-                if(node.left != null) queue.offer(node.left);
-                if(node.right != null) queue.offer(node.right);
-            }
-            
-            result.add(innerList);
-        }
-        return result;
-    }
-    
-    public void print(List<List<int[]>> elements) {
-        for (List<int[]> inner : elements) {
-            System.out.print("[");
-            for (int[] val : inner) {
-                System.out.print("(" + val[0] + ", " + val[1] + ", " + val[2] + ") ");
-            }
-            System.out.println("]");
-        }
-        
+    public int findMin(int[] arr, int start, int end) {
+        int idx = findMinIndex(arr, start, end, 0, arr.length - 1, 0);
+        return idx == -1 ? -1 : arr[idx];
     }
 }
