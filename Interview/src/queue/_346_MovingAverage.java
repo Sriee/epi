359fa34883354e77354f7fadc7bfca527b255082
sriee <psg.sriee@gmail.com>
2021-04-13 04:56:12 -0400

    Calculate moving average in a data stream: Built in queue

--- Interview/src/queue/_346_MovingAverage.java
+++ Interview/src/queue/_346_MovingAverage.java
@@ -1,11 +1,16 @@
 package queue;
 
+import java.util.*;
+
 public class _346_MovingAverage {
 
     // Variables for Ring Buffer approach
     private int size, capacity, head, tail;
     private int[] arr;
     private double sum;
+
+    // Variables for Built-in queue approach
+    private Queue<Integer> queue;
 
     public _346_MovingAverage(int size) {
         capacity = 0;
@@ -14,6 +19,8 @@ public class _346_MovingAverage {
         sum = 0;
         this.size = size;
         arr = new int[size];
+
+        queue = new LinkedList<>();
     }
 
     public double next(int val) {
@@ -34,11 +41,25 @@ public class _346_MovingAverage {
         return sum / capacity;
     }
 
+    // Built-in Queue Approach
+    private double nextQ(int val) {
+        if (queue.size() == size)
+            sum -= queue.poll();
+
+        queue.offer(val);
+        sum += val;
+        return sum / queue.size();
+    }
+
     public static void main(String[] args) {
         _346_MovingAverage mavg = new _346_MovingAverage(4);
 
         int[] vals = {1, 10, 3, 5, 6, -168845, 57832, -48, 100000};
 
+        for (int val : vals) System.out.print(mavg.next(val) + " ");
+        System.out.println();
+
+        mavg.sum = 0;
         for (int val : vals) System.out.print(mavg.next(val) + " ");
         System.out.println();
     }
