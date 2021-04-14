5e24fb72ebd064e9823d7d8bb3824b5b18fdae20
sriee <psg.sriee@gmail.com>
2021-04-14 05:54:48 -0400

    Perfect squares: BFS approach

--- Interview/src/dfs/_279_PerfectSquares.java
+++ Interview/src/dfs/_279_PerfectSquares.java
@@ -1,62 +1,57 @@
-package dfs;
 
-import java.util.*;
 
 /**
- * Did not solve. TLE. Memoization does not help. The following recursive solution won't work. Because the number of
- * steps computed is not optimal
+ * Did not solve.
  * <p>
- * Example: For n = 269
- * <p>
- * Optimal steps = 2; 100 + 169
- * Our solution returns = 3; 256 + 9 + 4
+ * Recursive Approach - TLE Since we are considering all combination. Intuition of using combination is right but for
+ * numbers > 10000 the stack is overflowing. Memoization does not help either.
  */
 public class _279_PerfectSquares {
-    int[] squares;
-    int answer = Integer.MAX_VALUE;
-    Map<Integer, Integer> map = new HashMap<>();
 
-    public int numSquares(int n) {
-        int max = (int) Math.sqrt(n);
-        squares = new int[max + 1];
+    /**
+     * BFS Approach: Trick is to identify the relation ship between current element to next element.
+     */
+    private int numSquares(int n) {
+        int max = (int) Math.sqrt(n), curr, next, step = 0;
+        int[] sq = new int[max + 1];
 
         for (int i = 1; i <= max; i++)
-            squares[i] = i * i;
+            sq[i] = i * i;
 
-        System.out.println(Arrays.toString(squares) + "\n");
-        // dfs(n, 0, 0);
-        return answer;
-    }
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(0);
 
-    private void dfs(int n, int soFar, int steps) {
-        if (soFar == n) {
-            answer = Math.min(answer, steps);
-            return;
-        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            step++;
 
-        for (int i = squares.length - 1; i > 0; i--) {
-            int curr = soFar + squares[i];
-            if (curr > n)
-                continue;
+            for (int k = 0; k < size; k++) {
+                curr = queue.poll();
 
-            int rest = n - curr;
-            if (map.containsKey(rest)) {
-                System.out.println("Am I " + curr + " " + map.get(rest));
-                answer = Math.min(answer, steps + map.get(rest));
-            } else {
-                System.out.println("Else " + curr + " " + (steps + 1));
-                map.put(curr, steps + 1);
-                dfs(n, curr, steps + 1);
+                for (int i = 1; i <= max; i++) {
+                    next = curr + sq[i];
+
+                    if (next > n)
+                        continue;
+                    else if (next == n)
+                        return step;
+                    else
+                        queue.offer(next);
+                }
             }
-
-            if (answer != Integer.MAX_VALUE)
-                break;
         }
+
+        return step;
     }
 
     public static void main(String[] args) {
         _279_PerfectSquares ps = new _279_PerfectSquares();
+        int[] nums = {19, 169, 5172, 9999};
+        int[] expected = {3, 1, 3, 4};
 
-        System.out.println(ps.numSquares(269));
+        // To enable assertions in a java, add "-ea" flag to javac compiler
+        for (int i = 0; i < nums.length; i++) {
+            assert ps.numSquares(nums[i]) == expected[i] : String.format("Assertion check failed for i = %d", i);
+        }
     }
 }
