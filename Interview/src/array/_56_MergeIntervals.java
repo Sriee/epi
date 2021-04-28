a5788a4338ac098e0748f8aa2fb2e12afcba1758
sriee <psg.sriee@gmail.com>
2021-04-28 05:38:01 -0400

    Optimizations to the merge interval pattern

--- Interview/src/array/_56_MergeIntervals.java
+++ Interview/src/array/_56_MergeIntervals.java
@@ -2,55 +2,72 @@ package array;
 
 import java.util.*;
 
+/**
+ * Merge Interval Pattern
+ *
+ * @see <a href=https://www.educative.io/courses/grokking-the-coding-interview>
+ * Grokking the Coding Interview: Patterns for Coding Questions</a>
+ * @see <a href=https://leetcode.com/problems/merge-intervals/discuss/21222/A-simple-Java-solution>
+ * LC Discuss: Merge Interval Pattern</a>
+ */
 public class _56_MergeIntervals {
 
     public int[][] merge(int[][] intervals) {
         if (intervals.length < 2)
             return intervals;
 
-        // Sort Intervals
+        // Sort Interval
         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
-        List<int[]> res = new ArrayList<>();
-        int i;
-        int[] prev = intervals[0];
-        for (i = 1; i < intervals.length; i++) {
-            int[] curr = intervals[i];
+        List<int[]> result = new ArrayList<>();
 
-            if (curr[0] > prev[1]) {
-                res.add(new int[]{prev[0], prev[1]});
-                prev = curr;
-            } else {
-                prev[1] = Math.max(prev[1], curr[1]);
+        // This trick will avoid the problem of dealing with the last interval
+        int[] prev = intervals[0];
+        result.add(prev);
+
+        for (int[] ivl : intervals) {
+            if (ivl[0] > prev[1]) { // Disjoint Intervals: Add new interval to the list
+                prev = ivl;
+                result.add(prev);
+            } else {    // Overlapping intervals: Extend the end of current interval
+                prev[1] = Math.max(prev[1], ivl[1]);
             }
         }
-        // Last Interval
-        res.add(new int[]{prev[0], prev[1]});
 
-
-        // Result
-        int[][] merged = new int[res.size()][2];
-        i = 0;
-
-        for (int[] ivl : res) {
-            merged[i][0] = ivl[0];
-            merged[i][1] = ivl[1];
-            i++;
-        }
-
-        return merged;
+        // Built-in method to convert List<int[]> to 2D array
+        return result.toArray(new int[result.size()][2]);
     }
 
     public static void main(String[] args) {
         _56_MergeIntervals mi = new _56_MergeIntervals();
-        int[][] merged, input;
-        input = new int[][]{
-                {1, 3},
-                {2, 6},
-                {8, 10},
-                {15, 18}
+        int[][] merged;
+        int[][][] input = {
+                {
+                        {1, 3},
+                        {2, 6},
+                        {8, 10},
+                        {15, 18}
+                },
+                {
+                        {15, 65},
+                        {36, 98},
+                        {4, 7},
+                        {5, 7},
+                        {68, 90},
+                        {76, 125},
+                        {637, 834},
+                        {0, 3},
+                        {90, 457},
+                        {6, 6},
+                        {5, 14},
+                        {536, 840},
+                        {95, 256}
+                }
         };
-        merged = mi.merge(input);
-        System.out.println(Arrays.deepToString(merged));
+
+        for (int[][] inp : input) {
+            merged = mi.merge(inp);
+            System.out.println(Arrays.deepToString(merged));
+        }
     }
 }
