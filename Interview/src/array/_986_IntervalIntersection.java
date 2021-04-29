3de7571f90a3feb38e9e780c62016bf69ca3af8a
sriee <psg.sriee@gmail.com>
2021-04-29 05:41:08 -0400

    Interval Intersection - two pointer approach

--- Interview/src/array/_986_IntervalIntersection.java
+++ Interview/src/array/_986_IntervalIntersection.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class _986_IntervalIntersection {
 
-    /*
+    /**
      * Approach 1: Merge sorted array
      *
      * We followed Merge Sorted array pattern to solve this problem. Since we are creating
@@ -55,6 +55,58 @@ public class _986_IntervalIntersection {
         return result.toArray(new int[result.size()][]);
     }
 
+    /**
+     * Approach 2: Two Pointer
+     * <p>
+     * TC: O(n)
+     * SC: O(m + n)
+     */
+    public int[][] intervalIntersection2(int[][] firstList, int[][] secondList) {
+        if (firstList.length == 0 || secondList.length == 0)
+            return new int[][]{};
+
+        List<int[]> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < firstList.length && j < secondList.length) {
+            int[] a = firstList[i], b = secondList[j];
+
+            /*
+               Checking for overlap
+               as    ae     bs    be
+                |____|       |____|
+                   |            |
+                   bs           as
+
+                if the intervals overlap, we are only storing the overlapped portions of
+                both intervals.
+
+                Why can't we use b[0] <= a[1] here?
+                To use the merge interval patterns overlapping check the following condition
+                a.start <= b.start must be met. Due to the way we advance the first and second
+                list this might not be always the case.
+                Ex: First test case
+                i = 2 -> a = {13, 23}
+                j = 1 -> b = {8, 12}
+                b[0] <= a[1] will be true and will result in {13, 12} which is wrong.
+             */
+            if ((a[0] >= b[0] && a[0] <= b[1]) || (b[0] >= a[0] && b[0] <= a[1])) {
+                result.add(new int[]{
+                        Math.max(a[0], b[0]),
+                        Math.min(a[1], b[1])
+                });
+            }
+
+            // How do we know which two intervals to compare? By comparing their
+            // end times
+            if (a[1] < b[1])
+                i++;
+            else
+                j++;
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+
     private void print(int[][] arr) {
         for (int[] row : arr) System.out.print(Arrays.toString(row) + " ");
 
@@ -68,7 +120,7 @@ public class _986_IntervalIntersection {
         // 1
         first = new int[][]{{0, 2}, {5, 10}, {13, 23}, {24, 25}};
         second = new int[][]{{1, 5}, {8, 12}, {15, 24}, {25, 26}};
-        result = ii.intervalIntersection(first, second);
+        result = ii.intervalIntersection2(first, second);
         ii.print(result);
 
         // 2
@@ -78,7 +130,7 @@ public class _986_IntervalIntersection {
 
         // 3
         first = new int[][]{{1, 5}, {8, 12}};
-        result = ii.intervalIntersection(first, new int[][]{});
+        result = ii.intervalIntersection2(first, new int[][]{});
         ii.print(result);
 
         // 4
@@ -90,7 +142,7 @@ public class _986_IntervalIntersection {
         // 5
         first = new int[][]{{8, 15}};
         second = new int[][]{{2, 6}, {8, 10}, {12, 20}};
-        result = ii.intervalIntersection(first, second);
+        result = ii.intervalIntersection2(first, second);
         ii.print(result);
     }
 }
