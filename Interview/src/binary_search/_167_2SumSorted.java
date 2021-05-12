0a27e9d52f5402ddb19b29a8886312d9549f11b8
sriee <psg.sriee@gmail.com>
2021-05-12 04:53:16 -0400

    Two sum sorted - better binary search approach

--- Interview/src/binary_search/_167_2SumSorted.java
+++ Interview/src/binary_search/_167_2SumSorted.java
@@ -44,6 +44,45 @@ public class _167_2SumSorted {
         return result;
     }
 
+    /**
+     * Better Binary Search
+     * <p>
+     * TC: O(n)
+     * Worst case: O(n) when two indexes are at the center
+     * Best case: O(log n)
+     * <p>
+     * SC: O(1)
+     * <p>
+     * How to move the pointers? Ex: [2, 7, 11, 13, 15], target = 20
+     * left right mid  sum
+     * 0     4    2   17  17 < 20. We have to move the left pointer. We have 2 options mid or left + 1.
+     * mid + right = 26 > 20 so, we can't move our left boundary to mid.
+     * We choose left + 1
+     * <p>
+     * 1     4    2   22  22 > 20  We have to move the right pointer. We have 2 options mid or right - 1.
+     * left + mid = 7 + 11 = 18 < 20 so, we can't move our right boundary to mid.
+     * We choose right - 1
+     * <p>
+     * 1     3    2   20  20 == 20 We found the result!
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target)
+                return new int[]{left + 1, right + 1};
+            else if (sum < target)
+                left = (numbers[mid] + numbers[right] < target) ? mid : left + 1;
+            else
+                right = (numbers[left] + numbers[mid] > target) ? mid : right - 1;
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         _167_2SumSorted ts = new _167_2SumSorted();
         int[][] input = {
@@ -51,10 +90,11 @@ public class _167_2SumSorted {
                 {-1, 2, 100},
                 {1, 2, 3, 4, 4, 9, 56, 90}
         };
-        int[] targets = {20, 99, 8};
+        int[] targets = {20, 99, 8}, result;
 
         for (int i = 0; i < targets.length; i++) {
-            System.out.println(Arrays.toString(ts.twoSum(input[i], targets[i])));
+            result = ((i & 1) == 0) ? ts.twoSum(input[i], targets[i]) : ts.twoSum2(input[i], targets[i]);
+            System.out.println(Arrays.toString(result));
         }
     }
 }
