07b1308e2a585aad1cb55e9fd91a2c6c52536834
sriee <psg.sriee@gmail.com>
2021-05-15 06:24:59 -0400

    Improved tree map approach by using an array

--- Interview/src/binary_search/_719_KSmallestPair.java
+++ Interview/src/binary_search/_719_KSmallestPair.java
@@ -5,54 +5,6 @@ import java.util.*;
 // https://www.youtube.com/watch?v=HiSvEhLIaTI
 public class _719_KSmallestPair {
 
-    public int smallestDistancePair(int[] nums, int k) {
-        Arrays.sort(nums);
-        int count[] = new int[nums[nums.length - 1] + 1];
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                count[nums[j] - nums[i]]++;
-            }
-        }
-        System.out.println(Arrays.toString(nums) + "\n" + Arrays.toString(count));
-
-        int number = 0;
-        for (int i = 0; i < count.length; i++) {
-            number += count[i];
-            if (number >= k)
-                return i;
-        }
-
-        return -1;
-    }
-
-    public int smallestDistancePair2(int[] nums, int k) {
-        Arrays.sort(nums);
-        int lo = 0, hi = nums[nums.length - 1] - nums[0];
-
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            int j = 0, count = 0; //findNumPairs(nums, mid);
-
-            for (int i = 0; i < nums.length; i++) {
-                while (j < nums.length && nums[j] <= nums[i] + mid) {
-//                    System.out.println(nums[j] + " " + nums[i] + "+" + mid + " = " + (j + 1));
-                    j++;
-                }
-                count += j - i - 1;
-//                System.out.println("count= " + count);
-            }
-
-            if (count >= k) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-
-        return lo;
-    }
-
     /**
      * Modified TreeMap approach
      * <p>
@@ -62,7 +14,7 @@ public class _719_KSmallestPair {
      * <p>
      * Solution TLE As expected.
      */
-    public int smallestDistancePair3(int[] nums, int k) {
+    public int smallestDistancePair1(int[] nums, int k) {
         // O(n log n)
         Arrays.sort(nums);
 
@@ -91,11 +43,76 @@ public class _719_KSmallestPair {
         return -1;
     }
 
+    /**
+     * Count Array approach
+     * <p>
+     * TC: O(n log n) + O(n ^ 2) + O(m)
+     * SC: O(m)
+     * where m = m = nums[nums.length - 1] + 1
+     * <p>
+     * Solution accepted but the time complexity is huge.
+     */
+    public int smallestDistancePair2(int[] nums, int k) {
+        // O(n log n)
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        // Range of our count = max(nums) - min(nums)
+        // +1 to handle num[i] == num[j]  { 0 } or
+        //              max(nums) - 0 { max(nums) }
+        int[] count = new int[nums[n - 1] + 1];
+
+        // O(n^2)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                count[nums[j] - nums[i]]++;
+            }
+        }
+
+        int num = 0;
+
+        // Worst case: O(m)
+        for (int i = 0; i < count.length; i++) {
+            num += count[i];
+            if (num >= k)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public int smallestDistancePair3(int[] nums, int k) {
+        Arrays.sort(nums);
+        int lo = 0, hi = nums[nums.length - 1] - nums[0];
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int j = 0, count = 0; //findNumPairs(nums, mid);
+
+            for (int i = 0; i < nums.length; i++) {
+                while (j < nums.length && nums[j] <= nums[i] + mid) {
+//                    System.out.println(nums[j] + " " + nums[i] + "+" + mid + " = " + (j + 1));
+                    j++;
+                }
+                count += j - i - 1;
+//                System.out.println("count= " + count);
+            }
+
+            if (count >= k) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
     public static void main(String[] args) {
         _719_KSmallestPair sp = new _719_KSmallestPair();
 //        int[] nums = {39,5,6,0,8,15,20};
         int[] nums = {3, 4, 1, 2, 5, 3, 7, 5};
-        System.out.println(sp.smallestDistancePair3(nums, 11));
-//        System.out.println(sp.smallestDistancePair(nums, 11));
+        System.out.println(sp.smallestDistancePair1(nums, 11));
+        System.out.println(sp.smallestDistancePair2(nums, 11));
     }
 }
