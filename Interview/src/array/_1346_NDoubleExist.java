90da420a72cd203a40d6672106174c791e5630eb
sriee <psg.sriee@gmail.com>
2021-05-27 05:38:17 -0400

    Check If N and Its Double Exist - Add 2 approaches

--- Interview/src/array/_1346_NDoubleExist.java
+++ Interview/src/array/_1346_NDoubleExist.java
@@ -3,6 +3,7 @@ package array;
 import java.util.*;
 
 class _1346_NDoubleExist {
+    // Approach 1: Using HashMap
     public boolean checkIfExist(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
         int n = arr.length;
@@ -23,14 +24,27 @@ class _1346_NDoubleExist {
         return false;
     }
 
+    // Approach 2: Using Sets
+    public boolean checkIfExist2(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : arr) {
+            if (set.contains(2 * i) || (i % 2 == 0 && set.contains(i / 2)))
+                return true;
+
+            set.add(i);
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         _1346_NDoubleExist nd = new _1346_NDoubleExist();
         int[][] inputs = {
                 {10, 2, 5, 3},
                 {7, 1, 14, 11}
         };
-        for (int[] input : inputs) {
-            System.out.println(nd.checkIfExist(input));
-        }
+        System.out.println(nd.checkIfExist(inputs[0]));
+        System.out.println(nd.checkIfExist2(inputs[1]));
     }
 }
