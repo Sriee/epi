37412ef71094924f51df108f4a1cf582798f2f02
sriee <psg.sriee@gmail.com>
2021-06-12 04:51:08 -0400

    Add key to Binary Search Tree node

--- Interview/src/bst/BSTNode.java
+++ Interview/src/bst/BSTNode.java
@@ -1,16 +1,18 @@
 package bst;
 
 public class BSTNode {
-    int val;
+    int key, val;
     BSTNode left, right;
 
-    public BSTNode(int _val) {
-        this.val = _val;
+    public BSTNode(int k, int v) {
+        this.key = k;
+        this.val = v;
     }
 
     @Override
     public String toString() {
-        return String.format("Node(%d, [%s, %s])",
+        return String.format("Node(%d = %d, [%s, %s])",
+                this.key,
                 this.val,
                 this.left == null ? "null" : this.left.val,
                 this.right == null ? "null" : this.right.val
--- Interview/src/bst/BinarySearchTree.java
+++ Interview/src/bst/BinarySearchTree.java
@@ -1,99 +1,75 @@
 package bst;
 
-import java.util.*;
-
 public class BinarySearchTree {
+
     private BSTNode root;
 
-    public void add(int key) {
-        root = insert(root, key);
+    public void put(int key, int val) {
+        root = insert(root, key, val);
     }
 
-    private BSTNode insert(BSTNode node, int val) {
+    private BSTNode insert(BSTNode node, int key, int val) {
         if (node == null)
-            return new BSTNode(val);
+            return new BSTNode(key, val);
 
-        if (val > node.val)
-            node.right = insert(node.right, val);
-        else if (val < node.val)
-            node.left = insert(node.left, val);
+        if (key > node.key)
+            node.right = insert(node.right, key, val);
+        else if (key < node.key)
+            node.left = insert(node.left, key, val);
+        else
+            node.val = val;
 
         return node;
     }
 
-    public boolean contains(int key) {
-        return contains(root, key);
+    public int get(int key) {
+        return get(root, key);
     }
 
-    private boolean contains(BSTNode node, int val) {
+    private int get(BSTNode node, int key) {
         if (node == null)
-            return false;
+            return -1;
 
-        if (val > node.val)
-            return contains(node.right, val);
-        else if (val < node.val)
-            return contains(node.left, val);
+        if (key > node.key)
+            return get(node.right, key);
+        else if (key < node.key)
+            return get(node.left, key);
         else
-            return true;
+            return node.val;
     }
 
     public void remove(int key) {
         root = remove(root, key);
     }
 
-    private BSTNode remove(BSTNode node, int val) {
+    private BSTNode remove(BSTNode node, int key) {
         if (node == null)
             return null;
 
-        if (val > node.val)
-            node.right = remove(node.right, val);
-        else if (val < node.val)
-            node.left = remove(node.left, val);
+        if (key > node.key)
+            node.right = remove(node.right, key);
+        else if (key < node.key)
+            node.left = remove(node.left, key);
         else {
             if (node.right == null)
                 return node.left;
             else if (node.left == null)
                 return node.right;
             else {
-                node.val = getMin(node.right);
-                node.right = remove(node.right, node.val);
+                BSTNode minNode = getMin(node.right);
+                node.key = minNode.key;
+                node.val = minNode.val;
+                node.right = remove(node.right, node.key);
             }
         }
 
         return node;
     }
 
-    private int getMin(BSTNode node) {
+    private BSTNode getMin(BSTNode node) {
         while (node.left != null)
             node = node.left;
 
-        return node.val;
-    }
-
-    public List<BSTNode> getNodes() {
-        Queue<BSTNode> queue = new LinkedList<>();
-        List<BSTNode> result = new ArrayList<>();
-
-        if (root == null)
-            return result;
-
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                BSTNode node = queue.poll();
-
-                result.add(node);
-                if (node.left != null)
-                    queue.offer(node.left);
-
-                if (node.right != null)
-                    queue.offer(node.right);
-            }
-        }
-
-        return result;
+        return node;
     }
 }
