2f50010a01bb637a394fba2a0c9fd00c247793bd
sriee <psg.sriee@gmail.com>
2021-06-23 04:49:14 -0400

    Kth Largest XOR Value - Arr Approach

--- Interview/src/heaps/_1738_KthLargestXor.java
+++ Interview/src/heaps/_1738_KthLargestXor.java
@@ -40,6 +40,42 @@ class _1738_KthLargestXor {
         return pq.poll();
     }
 
+    /**
+     * Approach 2: Sorted Array.
+     * <p>
+     * This approach TC is more than Priority Queue Approach but OJ's reports a lesser run time.
+     * <p>
+     * Run Time: 64 ms.
+     * <p>
+     * TC: O(3mn) + O(mn log mn)
+     * SC: O(mn) + O(log mn)
+     */
+    public int kthLargestXorArr(int[][] matrix, int k) {
+        int m = matrix.length, n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] ^= matrix[i][j - 1];
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] ^= matrix[i - 1][j];
+            }
+        }
+
+        int[] arr = new int[m * n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i * n + j] = matrix[i][j];
+            }
+        }
+
+        Arrays.sort(arr);
+        return arr[arr.length - k];
+    }
+
     public static void main(String[] args) {
         _1738_KthLargestXor klx = new _1738_KthLargestXor();
         klx.rand = new Random();
@@ -60,6 +96,6 @@ class _1738_KthLargestXor {
                 {1, 0, 9},
                 {3, 4, 8}
         };
-        System.out.println(klx.kthLargestXorPQ(matrix, 10));
+        System.out.println(klx.kthLargestXorArr(matrix, 10));
     }
 }
