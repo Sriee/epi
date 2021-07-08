19edf5a50e860c25ce9b329a13ef912330fef804
sriee <psg.sriee@gmail.com>
2021-07-08 05:21:33 -0400

    Kth Largest element in a stream - BST approach

--- Interview/src/bst/_703_KthLargest.java
+++ Interview/src/bst/_703_KthLargest.java
@@ -1,30 +1,64 @@
 package bst;
 
-import java.util.*;
-
 class _703_KthLargest {
 
-    /*
-     * Classic Kth largest problem solved with Heap
+    /**
+     * This approach illustrates a technique on making use of additional variable on the TreeNode to solve problems.
      */
-    private PriorityQueue<Integer> pq;
-    private final int k;
+    TreeNode root;
+    final int k;
 
     public _703_KthLargest(int k, int[] nums) {
-        this.pq = new PriorityQueue<>();
         this.k = k;
-
         for (int i : nums)
-            this.add(i);
+            root = insert(root, i);
+    }
+
+    private TreeNode insert(TreeNode node, int val) {
+        if (node == null)
+            return new TreeNode(val);
+
+        node.count++;
+        if (val < node.val)
+            node.left = insert(node.left, val);
+        else
+            node.right = insert(node.right, val);
+
+        return node;
     }
 
     public int add(int val) {
-        this.pq.offer(val);
+        root = insert(root, val);
+        return kthLargest();
+    }
 
-        if (this.pq.size() > this.k)
-            pq.poll();
+    private int kthLargest() {
+        int ck = k;
+        TreeNode iter = root;
 
-        return pq.peek();
+        while (ck > 0) {
+            int pos = 1 + (iter.right != null ? iter.right.count : 0);
+
+            if (ck > pos) {
+                ck -= pos;
+                iter = iter.left;
+            } else if (ck < pos) {
+                iter = iter.right;
+            } else {
+                break;
+            }
+        }
+
+        return iter.val;
+    }
+
+    static class TreeNode {
+        int val, count = 1;
+        TreeNode left = null, right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
     }
 
     public static void main(String[] args) {
