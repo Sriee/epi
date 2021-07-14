46e3a7b9f6d7f1fd883557a91e1165419523e75a
sriee <psg.sriee@gmail.com>
2021-07-14 05:26:49 -0400

    Add recursive approach steps to construct BST from Preorder Traversal

--- Interview/src/bst/_1008_BSTfromPreorder.java
+++ Interview/src/bst/_1008_BSTfromPreorder.java
@@ -4,6 +4,21 @@ class _1008_BSTfromPreorder {
 
     /**
      * Since preorder traverses root, left, and right. Repeated call to insert to BST method gives us the BST.
+     * <p>
+     * The expected approach to construct BST from preorder traversal are as follows:
+     * - Given preorder traversal = [8,5,1,7,10,12]
+     * - We know that 8 will be the root node
+     * TreeNode node = new TreeNode(preorder[start]);
+     * <p>
+     * - Next, we have to know the number of nodes that belong to the left subtree. Since this is a BST the right's
+     * node val > current root's value.
+     * int nextGreaterIdx = find(preorder, start + 1, preorder[start]); // 10
+     * <p>
+     * - In this case it's 10. So [5,1,7] should be in the left subtree of 8 and rest of the elements [10,12]
+     * should be on the right subtree.
+     * <p>
+     * node.left = bstFromPreorderRecursive(preorder, start + 1, nextGreaterIdx - 1);
+     * node.right = bstFromPreorderRecursive(preorder, nextGreaterIdx, end);
      */
     public BSTNode bstFromPreorder(int[] preorder) {
         BSTNode root = null;
