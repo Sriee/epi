4e11566a90dcf7f7878d8a5c7c968f162703f1de
sriee <psg.sriee@gmail.com>
2021-07-15 04:51:26 -0400

    Singly linked list node

--- Interview/src/list/ListNode.java
+++ Interview/src/list/ListNode.java
@@ -1,30 +1,30 @@
 package list;
 
 public class ListNode<T extends Comparable<T>> implements Comparable<ListNode<T>> {
-    public T data;
+    public T val;
     public ListNode<T> next;
 
     public ListNode() {
-        this.data = null;
+        this.val = null;
         this.next = null;
     }
 
-    public ListNode(T data) {
-        this.data = data;
+    public ListNode(T val) {
+        this.val = val;
     }
 
-    public ListNode(T data, ListNode<T> next) {
-        this.data = data;
+    public ListNode(T val, ListNode<T> next) {
+        this.val = val;
         this.next = next;
     }
 
     @Override
     public int compareTo(ListNode<T> other) {
-        return this.data.compareTo(other.data);
+        return this.val.compareTo(other.val);
     }
 
     @Override
     public String toString() {
-        return "ListNode(" + data + ")";
+        return "ListNode(" + val + ")";
     }
 }
