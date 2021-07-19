f2a08e1cee303bd1636991e0b2b68770edf8b12d
sriee <psg.sriee@gmail.com>
2021-07-19 05:16:47 -0400

    Kth smallest element in a BST - Inorder approach

--- Interview/src/bst/_230_KthSmallest.java
+++ Interview/src/bst/_230_KthSmallest.java
@@ -3,8 +3,26 @@ package bst;
 import java.util.*;
 
 class _230_KthSmallest {
+    Random rand = new Random();
 
-    public int kthSmallest(BSTNode root, int k) {
+    public int inorderApproach(BSTNode root, int k) {
+        List<Integer> lst = new ArrayList<>();
+        inOrder(root, lst, k);
+        return lst.get(k - 1);
+    }
+
+    private void inOrder(BSTNode node, List<Integer> lst, int k) {
+        if (node != null && lst.size() < k) {
+            inOrder(node.left, lst, k);
+
+            if (lst.size() < k) {
+                lst.add(node.val);
+                inOrder(node.right, lst, k);
+            }
+        }
+    }
+
+    public int stackApproach(BSTNode root, int k) {
         // Note the use of deque for stack operations
         Deque<BSTNode> stack = new ArrayDeque<>();
         BSTNode curr = root;
@@ -24,6 +42,22 @@ class _230_KthSmallest {
         }
 
         return -1;
+    }
+
+    public int kthSmallest(BSTNode root, int k) {
+        int res = -1;
+        switch (rand.nextInt(2)) {
+            case 0:
+                System.out.println("Stack Approach");
+                res = stackApproach(root, k);
+                break;
+            case 1:
+                System.out.println(" Approach");
+                res = inorderApproach(root, k);
+                break;
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
