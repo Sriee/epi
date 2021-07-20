0918891ca1abcfcefaafe7ff160380bca8963841
sriee <psg.sriee@gmail.com>
2021-07-20 05:47:12 -0400

    add stack approach to verify preorder

--- Interview/src/bst/_255_VerifyPreorder.java
+++ Interview/src/bst/_255_VerifyPreorder.java
@@ -22,10 +22,26 @@ class _255_VerifyPreorder {
         return verify(preOrder, start + 1, mid - 1, low, root) && verify(preOrder, mid, end, root, high);
     }
 
+    public boolean stackApproach(int[] preorder) {
+        int top = -1, low = Integer.MIN_VALUE;
+
+        for (int i : preorder) {
+            if (i < low) return false;
+
+            while (top >= 0 && i > preorder[top])
+                low = preorder[top--];
+
+            preorder[++top] = i;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         _255_VerifyPreorder vp = new _255_VerifyPreorder();
 
         System.out.println(vp.verifyPreorder(new int[]{5, 2, 1, 3, 6}));
         System.out.println(vp.verifyPreorder(new int[]{5, 2, 6, 1, 3}));
+        System.out.println(vp.stackApproach(new int[]{8, 5, 1, 7, 10, 12}));
     }
 }
