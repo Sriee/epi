a0a2822f67f32647c39dfaae0f272755006ba509
sriee <psg.sriee@gmail.com>
2021-07-22 05:39:11 -0400

    Add inorder traversal approach

--- Interview/src/bst/_501_FindMode.java
+++ Interview/src/bst/_501_FindMode.java
@@ -3,13 +3,15 @@ package bst;
 import java.util.*;
 
 class _501_FindMode {
+    Random rand = new Random();
+
     /**
      * Naive Approach: Write the contents of BST to a map and find out mode
      */
     Map<Integer, Integer> map = new HashMap<>();
     int maxCount = 0;
 
-    public int[] findMode(BSTNode root) {
+    public int[] naiveApproach(BSTNode root) {
         preOrder(root);
 
         int num = 0, i = 0;
@@ -41,6 +43,67 @@ class _501_FindMode {
 
         preOrder(node.left);
         preOrder(node.right);
+    }
+
+    /**
+     * Inorder Approach - Technique 2
+     * <p>
+     * We make use of a prev pointer that keeps track of previous node in an inorder traversal.
+     */
+    Deque<Integer> modes;
+    int count = 1;
+    BSTNode prev = null;
+
+    public int[] inorderApproach(BSTNode root) {
+        modes = new ArrayDeque<>();
+        inorder(root);
+
+        int[] res = new int[modes.size()];
+        int i = 0;
+
+        for (int m : modes)
+            res[i++] = m;
+
+        return res;
+    }
+
+    private void inorder(BSTNode node) {
+        if (node == null)
+            return;
+
+        inorder(node.left);
+
+        if (prev != null) {
+            count = prev.val == node.val ? count + 1 : 1;
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            // Emptying the container is faster compared to creating a new one
+            modes.clear();
+            modes.offerLast(node.val);
+        } else if (count == maxCount) {
+            modes.offerLast(node.val);
+        }
+
+        prev = node;
+        inorder(node.right);
+    }
+
+    public int[] findMode(BSTNode root) {
+        int[] res = null;
+        switch (rand.nextInt(2)) {
+            case 0:
+                System.out.println("Naive Approach");
+                res = naiveApproach(root);
+                break;
+            case 1:
+                System.out.println("Inorder Approach");
+                res = inorderApproach(root);
+                break;
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
