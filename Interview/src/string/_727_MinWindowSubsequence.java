907ddffc573f9bbe6d619d64d1aef80eb0f8c887
sriee <psg.sriee@gmail.com>
2021-09-27 05:48:35 -0400

    Minimum Window Subsequence - Corrected the issue

--- Interview/src/string/_727_MinWindowSubsequence.java
+++ Interview/src/string/_727_MinWindowSubsequence.java
@@ -3,67 +3,59 @@ package string;
 public class _727_MinWindowSubsequence {
 
     /**
-     * The below code is failing for test case No 6. This could be due to pointer handling and could not find out
-     * the issue.
+     * Sliding Window Pattern
+     *
+     * TC: O(m * n) where m = len(s2) and n = len(s1)
+     * SC: O(1)
      */
-    public String minWindow(String s, String t) {
-        int len = Integer.MAX_VALUE;
-        String result = "";
-        char[] s1 = s.toCharArray(), s2 = t.toCharArray();
+    private String minWindow(String s1, String s2) {
+        int start = -1, len = Integer.MAX_VALUE, sLen = s1.length(), tLen = s2.length();
+        char[] s = s1.toCharArray(), t = s2.toCharArray();
 
-        for (int idx1 = 0, idx2 = 0; idx1 < s.length();) {
-            if (s1[idx1] == s2[idx2]) {
-                idx2++;
+        for (int sIdx = 0, tIdx = 0; sIdx < sLen; sIdx++) {
+            if (s[sIdx] == t[tIdx]) {
+                if (++tIdx == tLen) {
+                    int end = sIdx + 1;
 
-                if (idx2 == t.length()) {
-                    idx2--;
-                    int start = idx1 - 1, end = idx1;
-
-                    while (idx2 >= 0) {
-                        if (s1[start] == s2[idx2]) {
-                            idx2--;
-                        }
-
-                        start--;
+                    while (--tIdx >= 0) {
+                        while (s[sIdx--] != t[tIdx]) ;
                     }
 
-                    start++; idx2++;
+                    sIdx++;
+                    tIdx++;
 
-                    if (end - start < len) {
-                        len = end - start;
-                        result = s.substring(start, end);
-                        // System.out.println(len + " " + result);
+                    if (end - sIdx < len) {
+                        len = end - sIdx;
+                        start = sIdx;
                     }
-
-                    idx1 = end;
                 }
             }
-            idx1++;
         }
-        return result;
+
+        return start == -1 ? "" : s1.substring(start, start + len);
     }
 
     public static void main(String[] args) {
         _727_MinWindowSubsequence mws = new _727_MinWindowSubsequence();
         String[] s1 = {
-                "abcdebdde",
-                "fgrqsqsnodwmxzkzxwqegkndaa",
-                "michmznaitnjdnjkdsnmichmznait",
-                "afgegrwgwga",
-                "abababa",
-                "ffynmlzesdshlvugsigobutgaetsnjlizvqjdpccdylclqcbghhixpjihximvhapymfkjxyyxfwvsfyctmhwmfjyjidnfryiyajmtakisaxwglwpqaxaicuprrvxybzdxunypzofhpclqiybgniqzsdeqwrdsfjyfkgmejxfqjkmukvgygafwokeoeglanevavyrpduigitmrimtaslzboauwbluvlfqquocxrzrbvvplsivujojscytmeyjolvvyzwizpuhejsdzkfwgqdbwinkxqypaphktonqwwanapouqyjdbptqfowhemsnsl",
+            "abcdebdde",
+            "fgrqsqsnodwmxzkzxwqegkndaa",
+            "michmznaitnjdnjkdsnmichmznait",
+            "afgegrwgwga",
+            "abababa",
+            "ffynmlzesdshlvugsigobutgaetsnjlizvqjdpccdylclqcbghhixpjihximvhapymfkjxyyxfwvsfyctmhwmfjyjidnfryiyajmtakisaxwglwpqaxaicuprrvxybzdxunypzofhpclqiybgniqzsdeqwrdsfjyfkgmejxfqjkmukvgygafwokeoeglanevavyrpduigitmrimtaslzboauwbluvlfqquocxrzrbvvplsivujojscytmeyjolvvyzwizpuhejsdzkfwgqdbwinkxqypaphktonqwwanapouqyjdbptqfowhemsnsl",
         };
         String[] s2 = {
-                "bde",
-                "kzed",
-                "michmznait",
-                "aa",
-                "ba",
-                "ntimcimzah"
+            "bde",
+            "kzed",
+            "michmznait",
+            "aa",
+            "ba",
+            "ntimcimzah"
         };
 
         for (int i = 0; i < s1.length; i++) {
-            System.out.println(mws.minWindow(s1[i] , s2[i]));
+            System.out.println(mws.minWindow(s1[i], s2[i]));
         }
     }
 }
