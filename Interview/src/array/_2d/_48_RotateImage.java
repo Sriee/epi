0a309aed9710784a1b95c2494d1eac2cba8759ff
sriee <psg.sriee@gmail.com>
2021-09-29 05:23:22 -0400

    add helper method to print 2d array

--- Interview/src/array/_2d/_48_RotateImage.java
+++ Interview/src/array/_2d/_48_RotateImage.java
@@ -11,7 +11,6 @@ public class _48_RotateImage {
             matrix[i] = matrix[j];
             matrix[j] = temp;
         }
-        // print(matrix);
 
         // Transpose
         for (int i = 0; i < n; i++) {
@@ -23,9 +22,29 @@ public class _48_RotateImage {
         }
     }
 
+    private void print(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         _48_RotateImage ri = new _48_RotateImage();
-        int[][] mat = new int[][]{{1, 2, 3}, {4, 5, 6}, {7,8,9}};
+        int[][] mat;
+
+        // 1
+        mat = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         ri.rotate(mat);
+        ri.print(mat);
+        System.out.println("====================================\n");
+
+        // 2
+        mat = new int[][]{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        ri.rotate(mat);
+        ri.print(mat);
     }
 }
