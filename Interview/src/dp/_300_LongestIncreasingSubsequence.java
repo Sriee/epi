a108133720bb177aa1c47665d6a06a0e25c13c3e
sriee <psg.sriee@gmail.com>
2021-10-20 05:26:51 -0400

    Sub routine which prints the longest increasing subsequence

--- Interview/src/dp/_300_LongestIncreasingSubsequence.java
+++ Interview/src/dp/_300_LongestIncreasingSubsequence.java
@@ -1,6 +1,6 @@
 package dp;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class _300_LongestIncreasingSubsequence {
 
@@ -22,7 +22,40 @@ public class _300_LongestIncreasingSubsequence {
             res = Math.max(res, d[i]);
         }
 
-        System.out.println(Arrays.toString(d));
+        return res + 1;
+    }
+
+    /**
+     * Sub routine which prints the longest increasing subsequence.
+     */
+    private int lisSequence(int[] nums) {
+        int n = nums.length, res = Integer.MIN_VALUE, resIdx = -1;
+        int[] d = new int[n], parent = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && d[j] + 1 > d[i]) {
+                    d[i] = d[j] + 1;
+                    parent[i] = j;
+                }
+
+            }
+            if (d[i] > res) {
+                res = d[i];
+                resIdx = i;
+            }
+        }
+
+        if (resIdx == -1)
+            return res + 1;
+
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        queue.offer(nums[resIdx]);
+        for (int p = resIdx; p != 0; p = parent[p])
+            queue.offerFirst(nums[parent[p]]);
+
+        System.out.println(queue);
         return res + 1;
     }
 
@@ -30,5 +63,6 @@ public class _300_LongestIncreasingSubsequence {
         _300_LongestIncreasingSubsequence lis = new _300_LongestIncreasingSubsequence();
         int[] inp = {10, 22, 9, 33, 21, 50, 41, 60, 80};
         System.out.println(lis.lengthOfLIS(inp));
+        lis.lisSequence(inp);
     }
 }
