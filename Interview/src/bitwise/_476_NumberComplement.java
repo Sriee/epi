a5b4e4b5cae72267e5fe015beabe797bbbcd4357
sriee <psg.sriee@gmail.com>
2021-11-01 05:18:36 -0400

    add xor approach

--- Interview/src/bitwise/_476_NumberComplement.java
+++ Interview/src/bitwise/_476_NumberComplement.java
@@ -19,13 +19,27 @@ public class _476_NumberComplement {
         return ans;
     }
 
+    /**
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public int findComplementXor(int num) {
+        if (num == 0) return 1;
+
+        int bitCount = (int) Math.floor((int)(Math.log(num) / Math.log(2))) + 1;
+        int allBitsSet = (int) Math.pow(2, bitCount) - 1;
+        return num ^ allBitsSet;
+    }
+
     public static void main(String[] args) {
         _476_NumberComplement nc = new _476_NumberComplement();
-        int[] numbers = {5, 0, 1, 39, 2147483};
-           // results = {2, 1, 0, 24, 2046820}
+        int[] numbers = {5, 0, 1, 38, 2147483};
+           // results = {2, 1, 0, 25, 2046820}
 
+        int ans = -1;
         for (int num : numbers) {
-            System.out.printf("Complement of %d is %d", num, nc.findComplement(num));
+            ans = (num & 2) == 0 ? nc.findComplement(num) : nc.findComplementXor(num);
+            System.out.printf("Complement of %d is %d\n", num, ans);
         }
     }
 }
