d676c7d18194a7a8d6a070fa7a1a2f032f6afd0f
sriee <psg.sriee@gmail.com>
2021-11-04 06:08:50 -0400

    Time based kv store using binary search + map approach

--- Interview/src/design/_981_TimeBasedKVStore.java
+++ Interview/src/design/_981_TimeBasedKVStore.java
@@ -3,9 +3,12 @@ import java.util.*;
 
 class _981_TimeBasedKVStore {
 
-	Map<String, TreeMap<Integer, String>>  timeStampMap;
+	private Map<String, List<String>> valueMap;
+    private Map<String, List<Integer>> tsMap;
+
 	public _981_TimeBasedKVStore() {
-		timeStampMap = new HashMap<>();
+		valueMap = new HashMap<>();
+        tsMap = new HashMap<>();
 	}
 	//
 
@@ -13,48 +16,70 @@ class _981_TimeBasedKVStore {
 	 * Set TimeStamp data variables
 	 *
 	 * Let M be number of set function calls and L be the average length of Key and Value strings
-	 * 	 TC = O(M L log M)
-	 * 	 SC = O(M. L)
+	 * 	 TC = O(M L)
+	 * 	 SC = O(M L)
 	 */
-	public void setValue(String key, String value, int timestamp) {
-		TreeMap<Integer, String> tsMap = timeStampMap.get(key);
+    public void set(String key, String value, int timestamp) {
+        if (valueMap.containsKey(key)) {
+            valueMap.get(key).add(value);
+            tsMap.get(key).add(timestamp);
+        } else {
+            List<String> valueList = new ArrayList<>();
+            valueList.add(value);
+            valueMap.put(key, valueList);
 
-		if (tsMap == null) {
-			tsMap = new TreeMap<>();
-		}
-
-		tsMap.put(timestamp, value);
-		timeStampMap.put(key, tsMap);
-	}
+            List<Integer> tsList = new ArrayList<>();
+            tsList.add(timestamp);
+            tsMap.put(key, tsList);
+        }
+    }
 
 	/**
 	 * Get the value for the given key and timestamp
 	 *
 	 * Let N be number of get function calls and L be the average length of Key and Value strings
-	 * 	 TC = O(N (L log M + log M))
+	 * 	 TC = O(N L log M)
 	 * 	 SC = O(1)
 	 */
-	public String getValue(String key, int timeStamp) {
-		// Replace this placeholder return statement with your code
-		TreeMap<Integer, String> tsMap = timeStampMap.get(key);
-		String value = "";
+    public String get(String key, int timestamp) {
+        String value = "";
 
-		if (tsMap != null) {
-			Map.Entry<Integer, String> entry = tsMap.floorEntry(timeStamp);
-			if (entry != null) value = entry.getValue();
-		}
+        if (!valueMap.containsKey(key)) {
+            return value;
+        }
 
-		return value;
-	}
+        List<Integer> tsList = tsMap.get(key);
+        int start = 0, end = tsList.size();
+
+        if (tsList.isEmpty() || tsList.get(start) > timestamp) {
+            return value;
+        } else if (tsList.get(end - 1) <= timestamp) {
+            return valueMap.get(key).get(end - 1);
+        } else {
+            while (start < end) {
+                int mid = start + (end - start) / 2;
+
+                if (tsList.get(mid) == timestamp) {
+                    return valueMap.get(key).get(end - 1);
+                } else if (tsList.get(mid) < timestamp) {
+                    start = mid + 1;
+                 } else {
+                    end = mid;
+                }
+            }
+        }
+
+        return end == 0 ? value : valueMap.get(key).get(start - 1);
+    }
 
 	public static void main(String args[]) {
 		_981_TimeBasedKVStore ts = new _981_TimeBasedKVStore();
-		ts.setValue("foo", "bar", 2);
-		System.out.println(ts.getValue("foo", 1)); // ""
-		System.out.println(ts.getValue("foo", 3)); // "bar"
+		ts.set("foo", "bar", 2);
+		System.out.println(ts.get("foo", 1)); // ""
+		System.out.println(ts.get("foo", 3)); // "bar"
 
-		ts.setValue("foo", "bar2", 4);
-		System.out.println(ts.getValue("foo", 2)); // "bar"
-		System.out.println(ts.getValue("foo", 5)); // "bar2"
+		ts.set("foo", "bar2", 4);
+		System.out.println(ts.get("foo", 2)); // "bar"
+		System.out.println(ts.get("foo", 5)); // "bar2"
 	}
 }
