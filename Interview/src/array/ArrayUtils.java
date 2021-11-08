5e41fa43bbfa339252dee77ee9ce89b70c306167
sriee <psg.sriee@gmail.com>
2021-11-08 04:42:10 -0400

    Add helper utility to print 2D matrix

---
+++ Interview/src/array/ArrayUtils.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArrayUtils {
+
+    public static void print2D(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
