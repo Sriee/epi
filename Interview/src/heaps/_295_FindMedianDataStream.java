b643a402c7564643a97d15e85a2cfa37d672ec24
sriee <psg.sriee@gmail.com>
2021-11-10 06:59:29 -0400

    Find median in data stream - insertion sort approach

--- Interview/src/heaps/_295_FindMedianDataStream.java
+++ Interview/src/heaps/_295_FindMedianDataStream.java
@@ -3,13 +3,16 @@ package heaps;
 import java.util.*;
 
 /**
- * Approach 1: Sorting the input numbers
+ * Approach 2: Using insertion sort
  *
- * Straight forward implementation. We sort the numbers stored in the internal array each time findMedian gets called.
- * Receiving TLE
+ * We assume that the current list is already sorted. When a new number comes, we have to add it at it's right position
+ * How do we find the right position?
+ *   Since the list is sorted we use binary search to find the position.
  *
- * TC: O(n log n)
- * SC: O(n)
+ * TC: O(log n) + O(n) = O(n)
+ *     O(log n) - For binary search.
+ *     O(n) - The elements in the list should be shifted to make room for the new element.
+ * SC: O(n) - Space for holding n elements.
  */
 public class _295_FindMedianDataStream {
 
@@ -20,11 +23,27 @@ public class _295_FindMedianDataStream {
     }
 
     public void addNum(int num) {
-        lst.add(num);
+        /* Note: Collections.binarySearch work in a weird way.
+         *   if (element present in lst)
+         *      return its idx;
+         *   else
+         *      return -( insertion position ) - 1
+         *
+         * For example, lst = [1, 2, 3, 10, 20]
+         * Collections.binarySearch(lst, 10) will give 3
+         * Collections.binarySearch(lst, 13) will give -5
+         *      13 should be inserted at idx 4
+         *      - (insertion position) - 1  = -(4) - 1 = -5
+         */
+        int idx = Collections.binarySearch(lst, num);
+        if (idx < 0) {
+            idx = Math.abs(idx) - 1;
+        }
+
+        lst.add(idx, num);
     }
 
     public double findMedian() {
-        Collections.sort(lst);
         int n = lst.size();
 
         return (n & 1) == 1 ? lst.get(n / 2) : lst.get(n / 2 - 1) / 2.0 + lst.get(n / 2) / 2.0;
