3effea45ff9b59b9db5dc29cb8d1f2809692ec80
sriee <psg.sriee@gmail.com>
2021-11-29 06:03:52 -0400

    kth smallest element in sorted matrix - binary search approach

--- Interview/src/binary_search/_378_KthSmallestSortedMatrix.java
+++ Interview/src/binary_search/_378_KthSmallestSortedMatrix.java
@@ -15,7 +15,7 @@ public class _378_KthSmallestSortedMatrix {
      *
      * SC - O(n) - Total number of elements in the heap
      */
-    public int kthSmallest(int[][] matrix, int k) {
+    public int kthSmallestUsingHeap(int[][] matrix, int k) {
         int n = matrix.length;
         PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> matrix[a[0]][a[1]]));
 
@@ -36,6 +36,51 @@ public class _378_KthSmallestSortedMatrix {
         return -1;
     }
 
+    /**
+     * TC
+     * ==
+     * For normal binary search the TC is O(log n). We get log (n) because at each iteration the search space is reduced
+     * to half. Here the search space is max - min value, but for each iteration the search space varies. Even if the
+     * search space varies, we do bring it down in the next iteration. The complexity is O(log (max - min)).
+     *
+     * For each iteration we count the number of elements in the left half = O(n)
+     * Total TC = O(n log(max - min))
+     *
+     * SC- O(1)
+     */
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int low = matrix[0][0], high = matrix[n - 1][n - 1], mid;
+
+        while (low != high) {
+            mid = low + (high - low) / 2;
+
+            if (countLessThanMid(matrix, mid) < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+
+    private int countLessThanMid(int[][] matrix, int target) {
+        int n = matrix.length, count = 0;
+        int row = 0, col = n - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] <= target) {
+                count += col - 1;
+                row++;
+            } else {
+                col--;
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         _378_KthSmallestSortedMatrix ksm = new _378_KthSmallestSortedMatrix();
 
@@ -47,7 +92,7 @@ public class _378_KthSmallestSortedMatrix {
                 {12, 12, 13}
         };
 
-        System.out.println(ksm.kthSmallest(matrix, 4));
+        System.out.println(ksm.kthSmallestUsingHeap(matrix, 4));
         System.out.println(ksm.kthSmallest(matrix, 9));
     }
 }
