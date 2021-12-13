abd351a3cc689489ecd90d16ea88539357c8107e
sriee <psg.sriee@gmail.com>
2021-12-13 05:14:42 -0400

    Implement stack using queue - single queue approach

--- Interview/src/stack/_225_StackUsingQueue.java
+++ Interview/src/stack/_225_StackUsingQueue.java
@@ -3,55 +3,48 @@ package stack;
 import java.util.*;
 
 /**
- * Uses 2 queue approach
+ * Using single queue approach
  */
 public class _225_StackUsingQueue {
-    Queue<Integer> fq, sq;
+    Deque<Integer> queue;
 
     /**
      * Initialize your data structure here.
      */
-    public _225_StackUsingQueue() {
-        fq = new LinkedList<>();
-        sq = new LinkedList<>();
-    }
+    public _225_StackUsingQueue() { queue = new ArrayDeque<>();}
 
     /**
      * Push element x onto stack.
      */
     public void push(int x) {
-        fq.offer(x);
+        queue.offer(x);
     }
 
     /**
      * Removes the element on top of the stack and returns that element.
      */
-    public int pop() {
-        top();
-        return sq.remove();
-    }
+    public int pop() { return queue.removeLast(); }
 
     /**
      * Get the top element.
      */
-    public int top() {
-        if (!fq.isEmpty()) {
-            while (!fq.isEmpty()) {
-                sq.offer(fq.poll());
-
-                // Reverse
-                for (int i = 1; i < sq.size(); i++)
-                    sq.offer(sq.poll());
-            }
-        }
-
-        return sq.peek();
-    }
+    public int top() { return queue.getLast(); }
 
     /**
      * Returns whether the stack is empty.
      */
-    public boolean empty() {
-        return fq.isEmpty() && sq.isEmpty();
+    public boolean empty() { return queue.isEmpty(); }
+
+    public static void main(String[] args) {
+        _225_StackUsingQueue stack = new _225_StackUsingQueue();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.top());
+
+        stack.push(40);
+        System.out.println(stack.empty());
+        System.out.println(stack.pop());
     }
 }
