9950bc564f4e5b431d25ff94ad369f486eab645e
sriee <psg.sriee@gmail.com>
2022-01-29 05:02:04 -0400

    Add dfs recursive approach

--- Interview/src/graph/_323_ConnectedComponents.java
+++ Interview/src/graph/_323_ConnectedComponents.java
@@ -1,14 +1,57 @@
 package graph;
 
-import java.util.*;
 import util.PrintHypens;
+
+import java.util.*;
 
 public class _323_ConnectedComponents {
 
-    /**
-     * Approach 1: Iterative approach instead of recursion.
-     * Note: We are still doing a DFS on each vertex
-     * Run time: 8 ms
+    /* ===========================================================================================
+     * Approach 1: DFS Recursive Approach
+     *
+     * TC: O(V + E)
+     * SC: O(V + E)
+     * ===========================================================================================
+     */
+    public int countComponentsDFS(int n, int[][] edges) {
+        // Use adjacency list to represent a graph
+        List<List<Integer>> graph = new ArrayList<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+            graph.add(new ArrayList<>());
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] seen = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!seen[i]) {
+                dfs(i, graph, seen);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int node, List<List<Integer>> graph, boolean[] seen) {
+        seen[node] = true;
+        for (int neighbor : graph.get(node)) {
+            if (!seen[neighbor]) {
+                dfs(neighbor, graph, seen);
+            }
+        }
+    }
+
+    /* ===========================================================================================
+     * Approach 2: DFS Iterative Approach
+     *
+     * TC: O(V + E)
+     * SC: O(V + E)
+     * ===========================================================================================
      */
     public int countComponents(int n, int[][] edges) {
         int count = 0;
@@ -17,25 +60,25 @@ public class _323_ConnectedComponents {
         boolean[] visited = new boolean[n];
 
         // Build graph
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             graph[i] = new HashSet<>();
 
-        for(int[] pair : edges) {
+        for (int[] pair : edges) {
             graph[pair[0]].add(pair[1]);
             graph[pair[1]].add(pair[0]);
         }
 
         // DFS on each node
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 count++;
                 stack.push(i);
 
-                while(!stack.isEmpty()) {
+                while (!stack.isEmpty()) {
                     int vertex = stack.pop();
                     visited[vertex] = true;
-                    for(int neighbor : graph[vertex]) {
-                        if(!visited[neighbor]) {
+                    for (int neighbor : graph[vertex]) {
+                        if (!visited[neighbor]) {
                             stack.push(neighbor);
                         }
                     }
@@ -67,5 +110,4 @@ public class _323_ConnectedComponents {
             System.out.println(PrintHypens.generate());
         }
     }
-
 }
