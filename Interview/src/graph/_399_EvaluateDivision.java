92b36b109869f3a24172a03475ed4a6af425eb65
sriee <psg.sriee@gmail.com>
2022-02-05 05:46:29 -0400

    evaluate condition - dfs approach

--- Interview/src/graph/_399_EvaluateDivision.java
+++ Interview/src/graph/_399_EvaluateDivision.java
@@ -1,12 +1,20 @@
 package graph;
 
-import javafx.util.*;
+import javafx.util.Pair;
+
 import java.util.*;
 
 public class _399_EvaluateDivision {
+
+    /* ===========================================================================================
+     * Approach 1: Union Find
+     *
+     * Prefer to use DFS approach for this problem.
+     * ===========================================================================================
+     */
     Map<String, Pair<String, Double>> groupToWeightMap;
 
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+    public double[] calcEquationUF(List<List<String>> equations, double[] values, List<List<String>> queries) {
         groupToWeightMap = new HashMap<>();
 
         for (int i = 0; i < equations.size(); i++) {
@@ -69,6 +77,63 @@ public class _399_EvaluateDivision {
         }
     }
 
+    /* ===========================================================================================
+     * Approach 2: Graph DFS
+     * ===========================================================================================
+     */
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+        for (int i = 0; i < values.length; i++) {
+            List<String> equation = equations.get(i);
+            String dividend = equation.get(0), divisor = equation.get(1);
+
+            graph.putIfAbsent(dividend, new HashMap<>());
+            graph.putIfAbsent(divisor, new HashMap<>());
+
+            graph.get(dividend).put(divisor, values[i]);
+            graph.get(divisor).put(dividend, 1 / values[i]);
+        }
+
+        double[] result = new double[queries.size()];
+        int idx = 0;
+        for (List<String> query : queries) {
+            String dividend = query.get(0), divisor = query.get(1);
+
+            if (!graph.containsKey(dividend) || !graph.containsKey(divisor)) {
+                result[idx] = -1.0;
+            } else if (dividend.equals(divisor)) {
+                result[idx] = 1.0;
+            } else {
+                result[idx] = dfs(dividend, divisor, 1.0, graph, new HashSet<>());
+            }
+            idx++;
+        }
+
+        return result;
+    }
+
+    private double dfs(String currentNode, String targetNode, double prod, Map<String, Map<String, Double>> graph, Set<String> visited) {
+        visited.add(currentNode);
+        double res = -1;
+
+        Map<String, Double> neighbors = graph.get(currentNode);
+        if (neighbors.containsKey(targetNode)) {
+            res = prod * neighbors.get(targetNode);
+        } else {
+            for (String neighbor : neighbors.keySet()) {
+                if (visited.contains(neighbor))
+                    continue;
+
+                res = dfs(neighbor, targetNode, prod * neighbors.get(neighbor), graph, visited);
+                if (res != -1)
+                    break;
+            }
+        }
+
+        visited.remove(currentNode);
+        return res;
+    }
+
     public static void main(String[] args) {
         _399_EvaluateDivision ed = new _399_EvaluateDivision();
 
@@ -77,14 +142,29 @@ public class _399_EvaluateDivision {
                 Arrays.asList("b", "c")
         );
         List<List<String>> queries = List.of(
-                Arrays.asList("a","c"),
-                Arrays.asList("b","a"),
-                Arrays.asList("a","e"),
-                Arrays.asList("a","a"),
-                Arrays.asList("x","x")
+                Arrays.asList("a", "c"),
+                Arrays.asList("b", "a"),
+                Arrays.asList("a", "e"),
+                Arrays.asList("a", "a"),
+                Arrays.asList("x", "x")
         );
 
-        double[] result = ed.calcEquation(equations, new double[] { 2.0, 3.0 }, queries);
+        double[] result = ed.calcEquationUF(equations, new double[]{2.0, 3.0}, queries);
+        System.out.println(Arrays.toString(result));
+
+        // Test case 2
+        equations = List.of(
+                Arrays.asList("a", "b"),
+                Arrays.asList("b", "c"),
+                Arrays.asList("bc", "cd")
+        );
+        queries = List.of(
+                Arrays.asList("a", "c"),
+                Arrays.asList("c", "b"),
+                Arrays.asList("bc", "cd"),
+                Arrays.asList("cd", "bc")
+        );
+        result = ed.calcEquation(equations, new double[]{1.5,2.5,5.0}, queries);
         System.out.println(Arrays.toString(result));
     }
 }
