8248562a2e8cc3c053978e926bd8d71440a4f520
sriee <psg.sriee@gmail.com>
2022-02-17 05:42:22 -0400

    min cost to connect all points - kruskal algorithm

--- Interview/src/graph/_1584_MinCostPoints.java
+++ Interview/src/graph/_1584_MinCostPoints.java
@@ -14,6 +14,71 @@ public class _1584_MinCostPoints {
             this.weight = _w;
         }
     }
+
+    int[] root, rank;
+
+    /* ===========================================================================================
+     * Approach 1: Minimum Spanning Tree - Kruskal Algorithm
+     * ===========================================================================================
+     */
+    public int minCostConnectPoints(int[][] points) {
+        PriorityQueue<Point> graph = new PriorityQueue<>((a, b) -> Integer.compare(a.weight, b.weight));
+        int n = points.length;
+
+        root = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            root[i] = i;
+            rank[i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                graph.offer(new Point(i, j, manhattanDistance(points[i], points[j])));
+            }
+        }
+
+        int cost = 0, numComponents = n;
+
+        while (!graph.isEmpty() && numComponents > 1) {
+            Point edge = graph.poll();
+            if (union(edge.x, edge.y)) {
+                cost += edge.weight;
+                numComponents--;
+            }
+        }
+
+        return cost;
+    }
+
+    private int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+
+        return root[x] = find(root[x]);
+    }
+
+    private boolean union(int x, int y) {
+        int rootX = find(x), rootY = find(y);
+
+        if (rootX == rootY)
+            return false;
+
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            root[rootX] = rootY;
+        } else {
+            root[rootY] = rootX;
+            rank[rootX]++;
+        }
+
+        return true;
+    }
+
+    /* ===========================================================================================
+     * Approach 2: Minimum Spanning Tree - Prims Algorithm
+     * ===========================================================================================
+     */
     public int minCostConnectPointsPrims(int[][] points) {
         List<List<Point>> graph = new ArrayList<>();
         int n = points.length;
@@ -64,10 +129,16 @@ public class _1584_MinCostPoints {
                 {{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}},
                 {{-1, 11}, {-2, 2}, {3, 5}, {4, 10}}
         };
+        int res;
 
         for (int i = 0; i < points.length; i++) {
             System.out.println((i + 1) + ".\tPoints: " + Arrays.deepToString(points[i]));
-            System.out.println("\tMinimum cost to connect all points= " + mcp.minCostConnectPointsPrims(points[i]));
+            if (i == 0)
+                res = mcp.minCostConnectPointsPrims(points[i]);
+            else
+                res = mcp.minCostConnectPoints(points[i]);
+
+            System.out.println("\tMinimum cost to connect all points= " + res);
             System.out.println(PrintHypens.generate());
         }
     }
