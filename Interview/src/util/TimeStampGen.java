51d6f8efe26a196aa8a2fe71544455f151d2ff9a
sriee <psg.sriee@gmail.com>
2022-03-02 05:26:43 -0400

    update timsestamp of time generator util

--- Interview/src/util/TimeStampGen.java
+++ Interview/src/util/TimeStampGen.java
@@ -6,7 +6,7 @@ public class TimeStampGen {
 
     public static void main(String[] args) {
         Random rand = new Random();
-        int n = 4, date = 27, mins, secs = 0;
+        int n = 3, date = 2, month = 3, mins, secs = 0;
         SortedSet<Integer> minSeen = new TreeSet<>();
         Set<Integer> secSeen = new HashSet<>();
 
@@ -29,8 +29,8 @@ public class TimeStampGen {
             mins = minIter.next();
             secs = secIter.next();
 
-            System.out.printf("GIT_AUTHOR_DATE='2021-10-%d 5:%02d:%02d -0400' GIT_COMMITTER_DATE='2021-10-%d " +
-                "5:%02d:%02d -0400' git commit -m ''\n", date, mins, secs, date, mins, secs);
+            System.out.printf("GIT_AUTHOR_DATE='2022-%02d-%02d 5:%02d:%02d -0400' GIT_COMMITTER_DATE='2022-%02d-%02d " +
+                "5:%02d:%02d -0400' git commit -m ''\n", month, date, mins, secs, month, date, mins, secs);
         }
     }
 }
